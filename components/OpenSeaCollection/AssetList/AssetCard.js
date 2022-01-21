6782c9e96ce348de94dfc44872bc28837445f4a8
joe lazarz <jlazarz87@gmail.com>
2022-01-21 13:45:36 -0500

    asset card layout

--- components/OpenSeaCollection/AssetList/AssetCard.js
+++ components/OpenSeaCollection/AssetList/AssetCard.js
@@ -1,9 +1,55 @@
 import React from "react"
+import Davatar from "@davatar/react"
 
-const AssetCard = () => {
+const AssetCard = ({ asset }) => {
   return (
-    <div className="w-full h-96 border border-white rounded-xl p-3">
-      Asset Card
+    <div className="flex flex-col w-full border border-white rounded-xl p-3">
+      <div className="flex flex-row justify-between">
+        {/* asset name & number of sales */}
+        <h1>{asset.name}</h1>
+        <span>Sales: {asset.num_sales}</span>
+      </div>
+      {/* asset img */}
+      <img src={asset.image_url} alt="" />
+      <div className="flex flex-row justify-between mt-2">
+        {/* owner avatar */}
+        <Davatar
+          size={40}
+          address={asset.owner}
+          generatedAvatarType="blockies"
+        />
+        {/* owner address */}
+        <span className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#0DB2AC] via-[#FC8D4D] to-[#FABA32]">
+          {asset.owner.substring(0, 6) +
+            "..." +
+            asset.owner.substring(
+              asset.owner.length - 5,
+              asset.owner.length - 1
+            )}
+        </span>
+        {/* asset price */}
+        <span>asset price</span>
+      </div>
+      <div className="flex flex-row justify-between mt-2">
+        {/* asset traits */}
+        <div className="grid grid-cols-3 gap-1">
+          {asset.traits.map((trait, index) => (
+            <div
+              key={index}
+              className="h-5 w-5 rounded-full bg-slate-300 dark:bg-slate-700"
+            ></div>
+          ))}
+        </div>
+      </div>
+      {/* buy offer buttons */}
+      <div className="flex flex-row justify-center mt-2">
+        <button className="bg-slate-200 dark:bg-slate-800 p-3 rounded mx-1">
+          buy
+        </button>
+        <button className="bg-slate-200 dark:bg-slate-800 p-3 rounded mx-1">
+          offer
+        </button>
+      </div>
     </div>
   )
 }
--- components/OpenSeaCollection/AssetList/index.js
+++ components/OpenSeaCollection/AssetList/index.js
@@ -1,15 +1,144 @@
 import React from "react"
 import AssetCard from "./AssetCard"
 
+const assets = [
+  {
+    token_id: "9999",
+    num_sales: 1,
+    background_color: null,
+    image_url:
+      "https://lh3.googleusercontent.com/CrSXeD3t60EdSZqBPSdzU82aA9zd5n5W5ap0Feg1efE7dB4NHjFU2sHTLAhem22Hezt9PSIPWFQUGoG_TJBzccwPGpzwyXoGbOHJtQ",
+    image_thumbnail_url:
+      "https://lh3.googleusercontent.com/CrSXeD3t60EdSZqBPSdzU82aA9zd5n5W5ap0Feg1efE7dB4NHjFU2sHTLAhem22Hezt9PSIPWFQUGoG_TJBzccwPGpzwyXoGbOHJtQ=s128",
+    image_original_url:
+      "https://ipfs.io/ipfs/QmVdZcdjS5fAzqzH4nMeKrqEhZM6Kjf6EgRDhxZEemEjJM",
+    animation_url: null,
+    animation_original_url: null,
+    name: "Doodle #9999",
+    permalink:
+      "https://opensea.io/assets/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/9999",
+    decimals: 0,
+    token_metadata:
+      "https://ipfs.io/ipfs/QmPMc4tcBsMqLRuCQtPmPe84bpSjrC3Ky7t3JWuHXYB4aS/9999",
+    owner: "0x4301b45f31bf7b511859c6be04bcef2c5100a112",
+    sell_orders: null,
+    creator: "0x2b3ab8e7bb14988616359b78709538b10900ab7d",
+    creator_status: "verified",
+    traits: [
+      {
+        trait_type: "head",
+        value: "yellow",
+        display_type: null,
+        max_value: null,
+        trait_count: 944,
+        order: null,
+      },
+      {
+        trait_type: "body",
+        value: "pink fleece",
+        display_type: null,
+        max_value: null,
+        trait_count: 317,
+        order: null,
+      },
+      {
+        trait_type: "face",
+        value: "aviators with mustache",
+        display_type: null,
+        max_value: null,
+        trait_count: 153,
+        order: null,
+      },
+      {
+        trait_type: "hair",
+        value: "blue messy",
+        display_type: null,
+        max_value: null,
+        trait_count: 250,
+        order: null,
+      },
+      {
+        trait_type: "background",
+        value: "blue",
+        display_type: null,
+        max_value: null,
+        trait_count: 726,
+        order: null,
+      },
+    ],
+  },
+  {
+    token_id: "9994",
+    background_color: null,
+    image_url:
+      "https://lh3.googleusercontent.com/4Cn6kanrELh7rinQWq6nrt4iJUsNnQGemt7Cd6O__gjuYTHqi-Je4LJuMu8SmYKGxvhhpMxnFwwPMGkZrYfvvPon_QN2sD2ZavUC8g",
+    image_thumbnail_url:
+      "https://lh3.googleusercontent.com/4Cn6kanrELh7rinQWq6nrt4iJUsNnQGemt7Cd6O__gjuYTHqi-Je4LJuMu8SmYKGxvhhpMxnFwwPMGkZrYfvvPon_QN2sD2ZavUC8g=s128",
+    image_original_url:
+      "https://ipfs.io/ipfs/QmYBYWMwGcyn1GoettFnn4yzpzRUwtaYrt3XscsxcyN6hD",
+    animation_url: null,
+    animation_original_url: null,
+    name: "Doodle #9994",
+    permalink:
+      "https://opensea.io/assets/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/9994",
+    decimals: 0,
+    token_metadata:
+      "https://ipfs.io/ipfs/QmPMc4tcBsMqLRuCQtPmPe84bpSjrC3Ky7t3JWuHXYB4aS/9994",
+    owner: "0x9d546e20cd75b62276787a1e4db2b9393633e2d6",
+    creator: "0x2b3ab8e7bb14988616359b78709538b10900ab7d",
+    creator_status: "verified",
+    traits: [
+      {
+        trait_type: "face",
+        value: "happy",
+        display_type: null,
+        max_value: null,
+        trait_count: 744,
+        order: null,
+      },
+      {
+        trait_type: "background",
+        value: "fire",
+        display_type: null,
+        max_value: null,
+        trait_count: 313,
+        order: null,
+      },
+      {
+        trait_type: "head",
+        value: "pink",
+        display_type: null,
+        max_value: null,
+        trait_count: 970,
+        order: null,
+      },
+      {
+        trait_type: "hair",
+        value: "purple cap",
+        display_type: null,
+        max_value: null,
+        trait_count: 279,
+        order: null,
+      },
+      {
+        trait_type: "body",
+        value: "yellow turtleneck",
+        display_type: null,
+        max_value: null,
+        trait_count: 354,
+        order: null,
+      },
+    ],
+  },
+]
+
 const AssetList = () => {
   return (
     <div className="flex w-full p-3">
       <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3 justify-items-center mx-4 w-full">
-        <AssetCard />
-        <AssetCard />
-        <AssetCard />
-        <AssetCard />
-        <AssetCard />
+        {assets.map((asset, index) => (
+          <AssetCard key={index} asset={asset} />
+        ))}
       </div>
     </div>
   )
