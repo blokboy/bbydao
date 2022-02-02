74faa7f61847e6576c9655e335c46f9bd9e596c6
Jordan Marsaw <jordanmarsaw@Jordans-MBP.lan>
2022-02-02 18:26:16 -0500

    lets check our provider object

--- utils/createSeaport.js
+++ utils/createSeaport.js
@@ -6,7 +6,7 @@ export const createSeaport = async () => {
   await window.ethereum.request({ method: "eth_requestAccounts" })
 
   const provider = new ethers.providers.Web3Provider(window.ethereum)
-
+  console.log('provider from seaport call ', provider);
   const seaport = new OpenSeaPort(provider, {
     networkName: Network.Main,
     apiKey: process.env.OPENSEA_API,
