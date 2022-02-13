e21aae1e456cc78d6b034121d34fb84c4b1a3446
joe lazarz <jlazarz87@gmail.com>
2022-02-13 12:28:41 -0500

    infinite scroll

--- components/OpenSeaCollection/AssetList/index.js
+++ components/OpenSeaCollection/AssetList/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import * as api from "query"
 import { useQuery } from "react-query"
+import { useInView } from "react-hook-inview"
 import AssetCard from "./AssetCard"
 
 const AssetList = ({ assets, address, slug }) => {
@@ -8,6 +9,14 @@ const AssetList = ({ assets, address, slug }) => {
   const [osAssets, setOsAssets] = React.useState([...assets])
   const [offset, setOffset] = React.useState(48)
   const [fetchAssets, setFetchAssets] = React.useState(false)
+  const [collectionComplete, setCollectionComplete] = React.useState(false)
+
+  const [ref, isVisible] = useInView(
+    {
+      onEnter: () => handleLoadMore(),
+    },
+    []
+  )
 
   let req = {
     slug: slug,
@@ -15,7 +24,7 @@ const AssetList = ({ assets, address, slug }) => {
     offset: offset,
   }
 
-  const { data, refetch } = useQuery(
+  const { data, isLoading, refetch } = useQuery(
     ["osAssets", slug],
     () => api.getOsAssets(req),
     {
@@ -26,7 +35,10 @@ const AssetList = ({ assets, address, slug }) => {
       staleTime: Infinity,
       onSuccess: async data => {
         const newAssets = await data?.assets
-        if (!newAssets.length) return
+        if (!newAssets.length) {
+          setCollectionComplete(true)
+          return
+        }
         setOsAssets([...osAssets, ...newAssets])
         setFetchAssets(false)
       },
@@ -34,17 +46,35 @@ const AssetList = ({ assets, address, slug }) => {
   )
 
   const handleLoadMore = () => {
+    if (collectionComplete) {
+      return
+    }
     setFetchAssets(true)
     setOffset(offset => (offset += 48))
     refetch()
   }
 
   return (
-    <div className="mx-4 grid w-full grid-cols-1 justify-items-center gap-3 p-3 md:grid-cols-2 lg:grid-cols-3">
-      {osAssets.map((asset, index) => (
-        <AssetCard key={index} asset={asset} />
-      ))}
-      <button onClick={handleLoadMore}>load more</button>
+    <div className="flex flex-col">
+      <div className="mx-4 grid w-full grid-cols-1 justify-items-center gap-3 p-3 md:grid-cols-2 lg:grid-cols-3">
+        {osAssets.map((asset, index) => (
+          <AssetCard key={index} asset={asset} />
+        ))}
+      </div>
+      {!collectionComplete ? (
+        <button
+          className="mt-10 flex w-full flex-col items-center justify-center"
+          ref={ref}
+          onClick={handleLoadMore}
+        >
+          <div className="motion-safe:animate-[bounce_3s_ease-in-out_infinite]">
+            <img alt="" src="/babydao.png" width={80} height={80} />
+          </div>
+          <span>loading...</span>
+        </button>
+      ) : (
+        <></>
+      )}
     </div>
   )
 }
