eba68e4b23123dd2cd0038284b1d700273dad8fc
Jordan Marsaw <jordanmarsaw@Jordans-MBP.lan>
2022-02-17 23:16:19 -0500

    cleaning up friend search logic and dropping wrapping search modals

--- components/UserDashboard/FriendsModal/FriendSearchCard.js
+++ components/UserDashboard/FriendsModal/FriendSearchCard.js
@@ -5,25 +5,26 @@ import { useAccount } from "wagmi"
 
 const FriendSearchCard = ({ friend, closeModal }) => {
   const [{ data, error, loading }] = useAccount()
+  console.log('friend ', friend)
 
   return (
     <div className="flex h-16 flex-row rounded bg-slate-300 px-3 shadow dark:bg-slate-800">
       <div className="flex h-full w-full flex-row items-center justify-between">
         <span className="text-lg font-semibold">
           @
-          {friend.initiatorEns
-            ? friend.initiatorEns
-            : `${friend.initiator.substring(0, 12) + "..."}`}
+          {friend.initiator === data?.address 
+            ? friend.targetEns || friend.target.substring(0, 12).concat("...")
+            : friend.initiatorEns || `${friend.initiator.substring(0, 12) + "..."}`}
         </span>
         <div className="flex flex-row">
-          {data?.address === friend.target ? (
+          {data?.address === friend.target || data?.address === friend.initiator ? (
             <button className="friend-result-btn" onClick={closeModal}>
               <HiOutlineChat />
             </button>
           ) : (
             <></>
           )}
-          <Link href={`/user/${encodeURIComponent(friend.initiator)}`}>
+          <Link href={`/user/${encodeURIComponent(friend.initiator === data?.address ? friend.target : friend.initiator)}`}>
             <a onClick={closeModal}>
               <button className="friend-result-btn">
                 <HiExternalLink />
