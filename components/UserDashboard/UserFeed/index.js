ed0b10dad2a5fd0a1dd19d7b72867c6a532cf710
Jordan Marsaw <jordanmarsaw@Jordans-MBP.attlocal.net>
2022-02-25 10:58:28 -0600

    cleaning up feed display

--- components/UserDashboard/UserFeed/index.js
+++ components/UserDashboard/UserFeed/index.js
@@ -1,7 +1,15 @@
 import React from "react"
+import { useAccount } from "wagmi"
 
-const UserFeed = () => {
-  return <div>UserFeed</div>
+const UserFeed = ({ address }) => {
+  const [{ data, error, loading }, disconnect] = useAccount()
+  return (
+  <div>
+    <h1 className="text-xl font-bold underline">
+        {data?.address === address ? "my feed" : "user feed"}
+    </h1>
+  </div>
+  )
 }
 
 export default UserFeed
