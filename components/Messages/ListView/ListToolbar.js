afc5aa6c389505a1af033f7fe4405d4323dbc240
Jordan Marsaw <jordanmarsaw@Jordans-MacBook-Pro.local>
2022-02-27 10:05:18 -0600

    removing messageViewToolbar in favor of a ListViewToolbar

--- components/Messages/ListView/ListToolbar.js
+++ components/Messages/ListView/ListToolbar.js
@@ -1,7 +1,17 @@
 import React from "react"
+import { BsChatDotsFill } from "react-icons/bs"
 
 const ListToolbar = () => {
-  return <div className="mb-2 flex h-[5%] w-full py-1 px-3">List Toolbar</div>
+  return (
+    <div className="mb-2 flex h-[5%] w-full">
+      <button className="flex flex-row rounded-xl bg-slate-50 py-1 px-3 text-sm shadow dark:bg-slate-800">
+        <span className="h-full pr-1 pt-1 text-slate-800 dark:text-white">
+          <BsChatDotsFill />
+        </span>
+        List Toolbar
+      </button>
+    </div>
+  )
 }
 
 export default ListToolbar
--- components/Messages/MessageView/index.js
+++ components/Messages/MessageView/index.js
@@ -1,12 +1,10 @@
 import React from "react"
 import MessageContent from "./MessageContent"
 import MessageForm from "./MessageForm"
-import MessageViewToolbar from "./MessageViewToolbar"
 
 const MessageView = () => {
   return (
     <div className="h-5/6 bg-slate-200 p-2 shadow-xl dark:bg-slate-900 md:w-[75%]">
-      <MessageViewToolbar />
       <MessageContent />
       <MessageForm />
     </div>
