db002916e6ebf60c5106bac915344b9aa21292f4
Jordan Marsaw <jordanmarsaw@Jordans-MacBook-Pro.local>
2022-02-27 11:44:38 -0600

    setting up the user feed card just need to adjust width

---
+++ components/UserDashboard/UserFeed/UserFeedCard.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { useAccount, useEnsLookup } from "wagmi"
+
+const UserFeedCard = () => {
+  const [{ data, error, loading }, disconnect] = useAccount()
+  const [
+    { data: ensData, error: ensError, loading: ensLoading },
+    lookupAddress,
+  ] = useEnsLookup({
+    address: data?.address,
+  })
+
+  if (!data) return null
+
+  return (
+    <div className="w-max cursor-pointer rounded-full border border-blue-100 bg-gradient-to-r from-[#0DB2AC] via-[#FC8D4D] to-[#FABA32] bg-clip-text px-4 py-2 font-semibold text-transparent shadow md:w-min">
+      Test content change to see if width is dynamic
+    </div>
+  )
+}
+
+export default UserFeedCard
--- components/UserDashboard/UserFeed/index.js
+++ components/UserDashboard/UserFeed/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { useAccount } from "wagmi"
+import UserFeedCard from "./UserFeedCard"
 
 const UserFeed = ({ address }) => {
   const [{ data, error, loading }, disconnect] = useAccount()
   return (
-  <div>
+  <div className="flex flex-col my-2">
     <h1 className="text-xl font-bold underline">
         {data?.address === address ? "my feed" : "user feed"}
     </h1>
+
+    <UserFeedCard />
   </div>
   )
 }
