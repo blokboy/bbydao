365f5f7779ecded15673fc4c93401cbf364f036e
Jordan Marsaw <jordanmarsaw@Jordans-MBP.lan>
2022-02-28 02:42:59 -0500

    getting ready to add in followers

--- components/Dao/SidePanel/index.js
+++ components/Dao/SidePanel/index.js
@@ -19,6 +19,14 @@ const SidePanel = ({ safeInfo, nftImage }) => {
         </span>
       </div>
 
+      <button
+      className="mr-3 my-4 flex w-max transform flex-row rounded-full bg-gradient-to-r from-[#0DB2AC] via-[#FC8D4D] to-[#FABA32] p-0.5 shadow transition duration-500 ease-in-out hover:-translate-x-0.5 hover:bg-white hover:bg-gradient-to-l dark:hover:bg-slate-700"
+      >
+      <span className="block rounded-full bg-slate-200 px-6 py-[0.45rem] font-bold text-[#FC8D4D] hover:bg-opacity-50 hover:text-white dark:bg-slate-900 dark:hover:bg-opacity-75">
+        be frens
+      </span>
+    </button>
+
       {/* make component to represent these (with pics) */}
       {/* modal pops to center of screen to scroll through all members */}
       <h1>members</h1>
