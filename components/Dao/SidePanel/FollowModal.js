2cb33e96c181534fc86528ed5e4c19fc1cf18d60
joe lazarz <jlazarz87@gmail.com>
2022-02-28 20:10:08 -0500

    handleSubmit ready

--- components/Dao/SidePanel/FollowModal.js
+++ components/Dao/SidePanel/FollowModal.js
@@ -3,6 +3,7 @@ import SafeServiceClient from "@gnosis.pm/safe-service-client"
 import useForm from "hooks/useForm"
 import { useUiStore } from "stores/useUiStore"
 import { useAccount } from "wagmi"
+import { HiX } from "react-icons/hi"
 
 const FollowModal = ({ safeAddress }) => {
   const followDaoModalOpen = useUiStore(state => state.followDaoModalOpen)
@@ -33,6 +34,18 @@ const FollowModal = ({ safeAddress }) => {
     setFollowDaoModalOpen()
   }
 
+  const handleSubmit = async e => {
+    e.preventDefault()
+    if (!state.safe) {
+      // timeout error on ui "please pick a safe"
+      console.log("no safe submitted")
+      return
+    }
+
+    console.log("following dao", safeAddress)
+    console.log("safe", state.safe)
+  }
+
   return (
     <div
       className="fixed inset-0 z-40 h-full w-full overflow-y-auto bg-slate-600 bg-opacity-50"
@@ -42,6 +55,11 @@ const FollowModal = ({ safeAddress }) => {
         className="z-50 mx-auto mt-24 flex w-full flex-col rounded-xl bg-slate-200 py-6 px-4 shadow dark:bg-slate-900 md:w-6/12"
         onClick={e => closeModal(e)}
       >
+        <div className="flex w-full justify-end">
+          <button className="modal-close-btn" onClick={e => closeModal(e)}>
+            <HiX />
+          </button>
+        </div>
         <span>{data?.address ? data.address : "not connected"}</span>
 
         <div className="mb-8">
@@ -63,6 +81,14 @@ const FollowModal = ({ safeAddress }) => {
                 </div>
               ))
             : "no safes"}
+          <div className="flex items-center justify-between">
+            <button
+              className="focus:shadow-outline w-full rounded-xl bg-slate-200 py-3 px-4 font-bold shadow-xl focus:outline-none dark:bg-slate-800"
+              onClick={handleSubmit}
+            >
+              submit
+            </button>
+          </div>
         </div>
       </div>
     </div>
