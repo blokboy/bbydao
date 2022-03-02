1c4f7391e4d0ed7c2cce7bd08f1986e30610d586
joe lazarz <jlazarz87@gmail.com>
2022-03-02 18:32:31 -0500

    create dao req setup

--- components/Forms/DaoForm.js
+++ components/Forms/DaoForm.js
@@ -82,7 +82,15 @@ const DaoForm = ({ address }) => {
     setTxWaiting(true)
     const bbyDao = await createBabyDao(e, ownerList)
     console.log("DaoForm.js bbyDao", bbyDao)
+    // const daoAddress = bbyDao.getAddress()
     // request to backend with dao info
+    // const req = {
+    //   name: state.name,
+    //   type: 1,
+    //   address: bbyDao.getAddress(),
+    //   members: ownerList,
+    // }
+    // mutateAsync(req)
     setTxWaiting(false)
     closeModal()
   }
