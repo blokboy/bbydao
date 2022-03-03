e533fcdcb21136df8c979799e10f071c7133ae77
Jordan Marsaw <jordanmarsaw@Jordans-MBP.lan>
2022-03-03 17:52:06 -0500

    feed populating

--- components/UserDashboard/UserFeed/index.js
+++ components/UserDashboard/UserFeed/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useAccount } from "wagmi"
 import { useQuery, useMutate } from "react-query"
 import UserFeedCard from "./UserFeedCard"
+import * as api from '../../../query';
 
 const UserFeed = ({ address }) => {
   const [{ data, error, loading }, disconnect] = useAccount()
@@ -13,13 +14,47 @@ const UserFeed = ({ address }) => {
       staleTime: 180000,
     }
   )
+
+  const following = []
+
+  if (friendData) {
+    for (const friend of friendData) {
+      if (friend.status === 4) {
+        following.push(friend.target)
+      } 
+    }
+  }
+
+  const { data: followData } = useQuery(
+    ["friends", following],
+    () => api.getUserFeed(following),
+    {
+      refetchOnWindowFocus: false,
+      staleTime: 180000,
+    }
+  )
+  console.log('following ', followData);
+
   return (
   <div className="flex flex-col my-2">
     <h1 className="text-xl font-bold underline">
         {data?.address === address ? "my feed" : "user feed"}
     </h1>
 
-    <UserFeedCard />
+    {
+      followData?.length ? (
+        followData.map( async (follower, index) => {
+          return (
+            <UserFeedCard key={index} follower={follower} />
+          )
+        })
+      ) : (
+      <span className="flex align-center rounded-full bg-slate-200 px-6 py-[0.45rem] font-bold text-[#FC8D4D] dark:bg-slate-900 dark:hover:bg-opacity-75">
+       Follow bbyDAOs to populate your feed!
+      </span>
+      )
+    }
+
   </div>
   )
 }
