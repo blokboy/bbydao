7599edab4815b44212605023c5e0fef3a96fc40a
0xburden <0xburden@protonmail.com>
2022-03-17 00:08:28 -0700

    finish network dropdown

--- components/Layout/Nav/DisconnectButton.js
+++ components/Layout/Nav/DisconnectButton.js
@@ -6,8 +6,9 @@ const DisconnectButton = () => {
 
   return (
     <button
-      className="mr-3 hidden w-max flex-row rounded-full bg-slate-100 px-4 py-2 shadow hover:bg-white hover:shadow-sm hover:shadow-orange-300 dark:bg-slate-900 dark:hover:bg-slate-800 dark:hover:shadow-sm dark:hover:shadow-orange-900 md:flex"
+      className="mr-3 hidden w-max cursor-pointer flex-row rounded-full bg-slate-100 px-4 py-2 shadow hover:bg-white hover:shadow-sm hover:shadow-orange-300 dark:bg-slate-900 dark:hover:bg-slate-800 dark:hover:shadow-sm dark:hover:shadow-orange-900 md:flex"
       onClick={disconnect}
+      type="button"
     >
       disconnect
     </button>
