014d060be1327fb6698d5bf8cf356427b6ed0f57
Théo Mode <milesxbridges@gmail.com>
2022-04-12 13:50:03 -0400

    get total ETH balance of babydao tokenlist

--- components/Dao/TokensNfts/TokenCard.js
+++ components/Dao/TokensNfts/TokenCard.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react"
-import { useAccount }                 from "wagmi"
+import React           from "react"
 import { FaEthereum }  from "react-icons/fa"
 import { useDaoStore } from "stores/useDaoStore"
+import { useAccount }  from "wagmi"
 import { isEmpty }     from "../../../utils/helpers"
 
 const TokenCard = ({ token, img }) => {
@@ -11,13 +11,14 @@ const TokenCard = ({ token, img }) => {
   const setUniswapLpModalOpen = useDaoStore(
     state => state.setUniswapLpModalOpen
   )
+  const setTxModalOpen = useDaoStore(state => state.setTxModalOpen)
   const lpToken0 = useDaoStore(state => state.lpToken0)
   const setLpToken0 = useDaoStore(state => state.setLpToken0)
   const lpToken1 = useDaoStore(state => state.lpToken1)
   const setLpToken1 = useDaoStore(state => state.setLpToken1)
   const isActive = lpToken0?.tokenAddress === token?.tokenAddress || lpToken1?.tokenAddress === token?.tokenAddress
   const setLpToken = () => {
-    if(!isActive) {
+    if (!isActive) {
       if (Object.keys(lpToken0).length === 0) {
         setLpToken0(token)
       }
@@ -39,9 +40,11 @@ const TokenCard = ({ token, img }) => {
   return (
     <div className={`w-full`}>
       <div className="flex flex-col rounded-lg bg-slate-100 p-1 shadow-inner dark:bg-slate-800">
-        <div className={`flex flex-row items-center justify-between rounded-lg bg-slate-300 p-2 shadow dark:bg-slate-900 ${isActive ? 'border border-teal-300' : ''}`}>
+        <div
+          className={`flex flex-row items-center justify-between rounded-lg bg-slate-300 p-2 shadow dark:bg-slate-900 ${isActive ? "border border-teal-300" : ""}`}>
           <div className="flex flex-row">
-            <div className="mx-2 flex h-10 w-10 items-center justify-center rounded-full border border-white bg-slate-200 dark:bg-slate-900">
+            <div
+              className="mx-2 flex h-10 w-10 items-center justify-center rounded-full border border-white bg-slate-200 dark:bg-slate-900">
               {token.token?.logoUri ? (
                 <img src={token.token.logoUri} />
               ) : (
@@ -49,7 +52,8 @@ const TokenCard = ({ token, img }) => {
               )}
             </div>
 
-            <span className="ml-4 flex w-28 flex-row items-center justify-center rounded bg-slate-100 p-1 text-[12px] dark:bg-slate-800">
+            <span
+              className="ml-4 flex w-28 flex-row items-center justify-center rounded bg-slate-100 p-1 text-[12px] dark:bg-slate-800">
               <span className="text-blue-500">
                 {(balance / 10 ** 18).toFixed(3)}{" "}
                 {token.token?.symbol ? token.token.symbol : "ETH"}{" "}
@@ -71,7 +75,7 @@ const TokenCard = ({ token, img }) => {
               <>
                 <button
                   className="mr-1 w-16 rounded-lg bg-blue-400 p-1 text-xs shadow-sm hover:bg-blue-500"
-                  // onClick={console.log("open list")}
+                  onClick={() => console.log("hii")}
                 >
                   send
                 </button>
@@ -89,7 +93,7 @@ const TokenCard = ({ token, img }) => {
               className="mr-1 w-16 rounded-lg bg-blue-400 p-1 text-xs shadow-sm hover:bg-blue-500"
               onClick={setLpToken}
             >
-              {isEmpty(lpToken0) && isEmpty(lpToken1) ? 'LP' : isActive ? 'Selected' : 'Pair'}
+              {isEmpty(lpToken0) && isEmpty(lpToken1) ? "LP" : isActive ? "Selected" : "Pair"}
             </button>
           </div>
         </div>
