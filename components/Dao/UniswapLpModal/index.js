ad01d6a8d332a4cef78a143414188da3cb4437bb
Théo Mode <milesxbridges@gmail.com>
2022-04-13 09:24:44 -0400

    update balance and set max

--- components/Dao/UniswapLpModal/index.js
+++ components/Dao/UniswapLpModal/index.js
@@ -1,10 +1,10 @@
-import useForm from "hooks/useForm"
-import React from "react"
-import { HiX } from "react-icons/hi"
+import useForm                    from "hooks/useForm"
+import React, { useMemo, useRef } from "react"
+import { HiX }                    from "react-icons/hi"
 import { Portal, PortalWithState } from "react-portal"
 import { useDaoStore } from "stores/useDaoStore"
 import Modal from "components/Layout/Modal"
-import { ChainId, Fetcher, WETH, Route } from "@uniswap/sdk"
+import { ChainId, Fetcher, WETH, Route, Pair, Token } from "@uniswap/sdk"
 
 const UniswapLpModal = ({ safeAddress }) => {
   const uniswapLpModalOpen = useDaoStore(state => state.uniswapLpModalOpen)
@@ -17,8 +17,8 @@ const UniswapLpModal = ({ safeAddress }) => {
   const setLpToken1 = useDaoStore(state => state.setLpToken1)
 
   // log token 0 and 1
-  console.log("lpToken0", lpToken0)
-  console.log("lpToken1", lpToken1)
+  // console.log("lpToken0", lpToken0)
+  // console.log("lpToken1", lpToken1)
 
   const { state, setState, handleChange } = useForm()
 
@@ -50,6 +50,55 @@ const UniswapLpModal = ({ safeAddress }) => {
     return null
   }
 
+  const selectedPair = useMemo(() => {
+    const token0 = new Token(
+      ChainId.MAINNET,
+      lpToken0?.tokenAddress,
+      lpToken0?.token?.decimals,
+      lpToken0?.token?.symbol,
+      lpToken0?.token?.name
+    );
+
+    const token1 = new Token(
+      ChainId.MAINNET,
+      lpToken1?.tokenAddress,
+      lpToken1?.token?.decimals,
+      lpToken1?.token?.symbol,
+      lpToken1?.token?.name
+    );
+
+    return [token0, token1]
+
+  }, [lpToken0, lpToken1])
+
+  const token0InputRef = useRef()
+  const token1InputRef = useRef()
+
+  const handleSetValue = (e, token) => {
+    const bal = token?.balance
+    const dec = token?.token?.decimals
+    const max =  bal / 10 ** dec
+    const input = e?.target?.valueAsNumber
+    const name = e?.target?.name
+
+    if(input > max) {
+      setState(state => ({ ...state, [name]: max }))
+    } else {
+      handleChange(e)
+    }
+  }
+
+  const readableBalance = (token) => {
+    return Number((token?.balance / 10 ** token?.token?.decimals).toString().match(/^\d+(?:\.\d{0,3})?/))
+  }
+
+  const setMax = (token, ref) => {
+    const name = ref?.current?.name
+    const max = ref?.current?.max
+    setState(state => ({ ...state, [name]: max }))
+  }
+
+
   return (
     <Modal close={closeUniswapLpModal} heading={"Uniswap LP"}>
       <form
@@ -60,7 +109,7 @@ const UniswapLpModal = ({ safeAddress }) => {
           <div className="flex flex-row">
             <input
               value={state?.token0}
-              onChange={handleChange}
+              onChange={(e) => handleSetValue(e, lpToken0)}
               className="h-16 w-full appearance-none rounded-lg bg-slate-100 py-2 px-3 text-xl leading-tight focus:outline-none dark:bg-slate-800"
               id="name"
               name="token0"
@@ -68,6 +117,8 @@ const UniswapLpModal = ({ safeAddress }) => {
               step={0.000001}
               placeholder="0.0"
               required
+              max={lpToken0?.balance / 10 ** lpToken0?.token?.decimals}
+              ref={token0InputRef}
             />
             {/* Button to select token  */}
             <div className="m-2 w-2/12 rounded-xl bg-slate-200 p-2 text-center text-xl dark:bg-slate-700">
@@ -76,7 +127,7 @@ const UniswapLpModal = ({ safeAddress }) => {
           </div>
           <div className="flex w-full flex-row justify-end space-x-2 px-2">
             <div>balance:</div>
-            <div>{(lpToken0?.balance / 10 ** 18).toFixed(3)}</div>
+            <div>{readableBalance(lpToken0)}</div>
           </div>
           <div className="mr-3 flex cursor-pointer justify-end text-[#FC8D4D]">
             max
@@ -87,7 +138,7 @@ const UniswapLpModal = ({ safeAddress }) => {
           <div className="flex flex-row">
             <input
               value={state?.token1}
-              onChange={handleChange}
+              onChange={(e) => handleSetValue(e, lpToken1)}
               className="h-16 w-full appearance-none rounded-lg bg-slate-100 py-2 px-3 text-xl leading-tight focus:outline-none dark:bg-slate-800"
               id="name"
               name="token1"
@@ -95,6 +146,8 @@ const UniswapLpModal = ({ safeAddress }) => {
               step={0.000001}
               placeholder="0.0"
               required
+              max={lpToken1?.balance / 10 ** lpToken1?.token?.decimals}
+              ref={token1InputRef}
             />
             {/* Button to select token  */}
             <div className="m-2 w-2/12 rounded-xl bg-slate-200 p-2 text-center text-xl dark:bg-slate-700">
@@ -103,9 +156,12 @@ const UniswapLpModal = ({ safeAddress }) => {
           </div>
           <div className="flex w-full flex-row justify-end space-x-2 px-2">
             <div>balance:</div>
-            <div>{(lpToken1?.balance / 10 ** 18).toFixed(3)}</div>
+            <div>{readableBalance(lpToken1)}</div>
           </div>
-          <div className="mr-3 flex cursor-pointer justify-end text-[#FC8D4D]">
+          <div
+            className="mr-3 flex cursor-pointer justify-end text-[#FC8D4D]"
+            onClick={() => setMax(lpToken1, token1InputRef)}
+          >
             max
           </div>
         </div>
