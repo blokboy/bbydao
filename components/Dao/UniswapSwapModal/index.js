1f8214b0ae8999f03203e349df6f4c566ed9d257
joe lazarz <jlazarz87@gmail.com>
2022-04-14 22:03:00 -0400

    working on gnosis provider for uniswap swap

--- components/Dao/UniswapSwapModal/index.js
+++ components/Dao/UniswapSwapModal/index.js
@@ -2,30 +2,55 @@ import React from "react"
 import { useTheme } from "next-themes"
 import Modal from "components/Layout/Modal"
 import { useDaoStore } from "stores/useDaoStore"
-import { chain, defaultChains } from "wagmi"
+import { chain, defaultChains, useSigner } from "wagmi"
 import { ethers } from "ethers"
+import useGnosisProxyContract from "hooks/useGnosisProxyContract"
 
 import { ChainId } from "@uniswap/sdk"
 import { SwapWidget } from "@uniswap/widgets"
 import "@uniswap/widgets/fonts.css"
 
-const UniswapSwapModal = () => {
-  const setUniswapSwapModalOpen = useDaoStore(state => state.setUniswapLpModalOpen)
+const UniswapSwapModal = ({ safeAddress }) => {
+  const setUniswapSwapModalOpen = useDaoStore(state => state.setUniswapSwapModalOpen)
   const closeUniswapSwapModal = () => {
     setUniswapSwapModalOpen(false)
   }
 
+  const [{data, loading}] = useSigner()
+  const [gnosisProvider, setGnosisProvider] = React.useState(null)
+  const getContract = () => {
+    const contract = new ethers.Contract(
+      safeAddress,
+      [
+        {
+          inputs: [{ internalType: "address", name: "_singleton", type: "address" }],
+          stateMutability: "nonpayable",
+          type: "constructor",
+        },
+        { stateMutability: "payable", type: "fallback" },
+      ]
+    )
+    const connected = contract.connect(data)
+    console.log('connected contract', connected)
+    setGnosisProvider(connected.provider)
+  }
+
+  React.useEffect(() => {
+    if (!loading && data) {
+      getContract()
+    }
+  }, [loading])
+
   // review all of this
   // how do we swap from the bbyDAO wallet
   const chainId = ChainId.MAINNET
-  const provider = new ethers.providers.Web3Provider(window.ethereum)
   // API key for Ethereum node
   const infuraId = process.env.INFURA_ID
   // Chains for connectors to support
   const chains = defaultChains
   const rpcUrl = chains.find(x => x.id === chainId)?.rpcUrls?.[0] ?? chain.mainnet.rpcUrls[0]
   const jsonRpcEndpoint = `${rpcUrl}/${infuraId}`
-  
+
   const { theme } = useTheme()
   const uniswapTheme = {
     dark: {
@@ -66,16 +91,26 @@ const UniswapSwapModal = () => {
     },
   }
 
+  if (gnosisProvider) {
+    console.log('gnosisProvider', gnosisProvider)
+
+    return (
+      <Modal close={closeUniswapSwapModal} heading={"Uniswap Swap"}>
+        <div className="Uniswap my-3">
+          <SwapWidget
+            width={"100%"}
+            provider={gnosisProvider}
+            jsonRpcEndpoint={jsonRpcEndpoint}
+            theme={theme === "light" ? uniswapTheme.light : uniswapTheme.dark}
+          />
+        </div>
+      </Modal>
+    )
+  }
+
   return (
     <Modal close={closeUniswapSwapModal} heading={"Uniswap Swap"}>
-      <div className="Uniswap my-3">
-        <SwapWidget
-          width={"100%"}
-          provider={provider}
-          jsonRpcEndpoint={jsonRpcEndpoint}
-          theme={theme === "light" ? uniswapTheme.light : uniswapTheme.dark}
-        />
-      </div>
+      <div>noope</div>
     </Modal>
   )
 }
