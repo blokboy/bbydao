05ff5ec2acf1504d126fc0a321a9b0a06f049e55
Théo Mode <milesxbridges@gmail.com>
2022-04-18 01:45:28 -0400

    resolve uniswap lp bug

--- components/Dao/UniswapLpModal/index.js
+++ components/Dao/UniswapLpModal/index.js
@@ -10,37 +10,15 @@ import {eToNumber}                                         from 'utils/helpers'
 
 
 const UniswapLpModal = ({safeAddress}) => {
-    const chainId = ChainId.MAINNET
     const uniswapLpModalOpen = useDaoStore(state => state.uniswapLpModalOpen)
-    const setUniswapLpModalOpen = useDaoStore(
-        state => state.setUniswapLpModalOpen
-    )
+    const setUniswapLpModalOpen = useDaoStore(state => state.setUniswapLpModalOpen)
     const lpToken0 = useDaoStore(state => state.lpToken0)
-    const setLpToken0 = useDaoStore(state => state.setLpToken0)
     const lpToken1 = useDaoStore(state => state.lpToken1)
     const setLpToken1 = useDaoStore(state => state.setLpToken1)
-
+    const setLpToken0 = useDaoStore(state => state.setLpToken0)
     const {state, setState, handleChange} = useForm()
 
-    /*  Create reference to input of token */
-    const token0InputRef = useRef()
-    const token1InputRef = useRef()
-    const [globalPair, setGlobalPair] = useState()
-
-    const init = async () => {
-        setState(state => ({...state, [token0InputRef?.current?.name]: 0}))
-        setState(state => ({...state, [token1InputRef?.current?.name]: 0}))
-        const uniPair = await Fetcher.fetchPairData(selectedTokens[token0InputRef?.current?.name], selectedTokens[token1InputRef?.current?.name])
-        await setGlobalPair(uniPair)
-    }
-
-    useEffect(() => {
-        init()
-
-
-    }, [])
-
-    // close uniswap lp modal
+    // Close function provided to <Modal /> component
     const closeUniswapLpModal = e => {
         setLpToken0({})
         setLpToken1({})
@@ -50,38 +28,31 @@ const UniswapLpModal = ({safeAddress}) => {
 
     const handleSubmit = async e => {
         e.preventDefault()
-        const daiToken = lpToken0?.token.symbol === "WETH" ? lpToken1 : lpToken0
-        const chainId = ChainId.MAINNET
-        const dai = await Fetcher.fetchTokenData(chainId, daiToken.tokenAddress)
-        const weth = WETH[chainId]
-        const pair = await Fetcher.fetchPairData(dai, weth)
-        const route = new Route([pair], weth)
-        console.log("route midprice", route?.midPrice.toSignificant(6))
-        console.log(
-            "route midprice invert",
-            route?.midPrice.invert().toSignificant(6)
-        )
-        console.log("route", route)
+
+        console.log('e', e)
     }
 
-    if ((!lpToken0 || !lpToken1) && uniswapLpModalOpen) {
-        setUniswapLpModalOpen()
-        return null
-    }
+    /*  Create reference to input of tokens and initialize pair */
+    const token0InputRef = useRef()
+    const token1InputRef = useRef()
+    const [pair, setPair] = useState()
+    const [liquidityInfo, setLiquidityInfo] = useState({})
+    const [maxError, setMaxError] = useState("")
 
-
-    const totalSupply = async (pair) => {
+    /* Get Total Supply of LP pair on-chain  */
+    const totalPairSupply = async (pair) => {
         /* create a generic provider and query for unsold market items */
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         const contract = new ethers.Contract(pair.liquidityToken.address, IUniswapV2ERC20.abi, provider)
-        const supply = await contract.totalSupply()
-
-        return supply
+        return await contract.totalSupply()
+    }
+    /* Human Readable Token Balance  */
+    const readableTokenBalance = (token) => {
+        return Number((token?.balance / 10 ** token?.token?.decimals).toString().match(/^\d+(?:\.\d{0,3})?/))
     }
 
-
     /*  Construct object of selected tokens represented as Uniswap Token Objects */
-    const selectedTokens = useMemo(() => {
+    const uniswapTokens = useMemo(() => {
         const token0 = new Token(
             ChainId.MAINNET,
             lpToken0?.tokenAddress,
@@ -98,98 +69,95 @@ const UniswapLpModal = ({safeAddress}) => {
             lpToken1?.token?.name
         )
 
-        return {token0, token1}
+        return {[lpToken0?.token?.symbol]: token0, [lpToken1?.token?.symbol]: token1}
 
     }, [lpToken0, lpToken1])
-
-
-    const readableBalance = (token) => {
-        return Number((token?.balance / 10 ** token?.token?.decimals).toString().match(/^\d+(?:\.\d{0,3})?/))
-    }
-
-    const handleSetValue = async (e, token, ref) => {
+    /* Handle setting token values and retrieving liquidity pair information  */
+    const handleSetTokenValue = async (e, token, tokenRef) => {
         const bal = token?.balance
         const dec = token?.token?.decimals
         const max = bal / 10 ** dec
-        const input = e?.target?.valueAsNumber
-        const name = e?.target?.name
-        const pairToken = name === "token0" ? lpToken1 : lpToken0
-        const pairTokenRef = name === "token0" ? token1InputRef : token0InputRef
-        const pairValue = Number((input * token?.fiatConversion) / pairToken?.fiatConversion)
+        const token0 = Object.entries(uniswapTokens).filter(item => item[0] === token.token.symbol)[0][1]
+        const token0Input = e?.target?.valueAsNumber
+        const route = new Route([pair], uniswapTokens[token.token.symbol])
+        const midPrice = route.midPrice.toSignificant(6)
+        const token1 = Object.entries(uniswapTokens).filter(item => item[0] !== token.token.symbol)[0][1]
+        const token1Input = Number((token0Input * midPrice))
 
-        if (input > max) {
-            setMax(token, ref)
+        if (token0Input > max) {
+            handleSetMaxTokenValue(token, tokenRef)
         } else {
-            setState(state => ({...state, [ref?.current?.name]: input}))
-            setState(state => ({...state, [pairTokenRef?.current?.name]: pairValue}))
+            setState(state => ({...state, [token.token.symbol]: token0Input}))
+            setState(state => ({...state, [token1?.symbol]: token1Input}))
             setMaxError("")
 
-            if (!isNaN(input) && !isNaN(pairValue) && input > 0 && pairValue > 0) {
-                const liquidityInfo = await getLiquidityTokenInfo({
-                    uniPair: globalPair,
-                    token0: token,
-                    token0Ref: ref,
-                    token0Input: input.toFixed(10),
-                    token1: pairToken,
-                    token1Ref: pairTokenRef,
-                    token1Input: pairValue.toFixed(10)
+            if (!isNaN(token0Input) && !isNaN(token1Input) && token0Input > 0 && token1Input > 0) {
+                const liquidityInfo = await getLiquidityPairInfo({
+                    pair: pair,
+                    token0: token0,
+                    token0Input: token0Input.toFixed(10),
+                    token1: token1,
+                    token1Input: token1Input.toFixed(10)
                 })
                 setLiquidityInfo(liquidityInfo)
             }
         }
     }
+    const handleSetMaxTokenValue = async (token, tokenRef) => {
+        const token0 = uniswapTokens[token.token.symbol]
+        const token0Input = tokenRef?.current?.max
+        const pairToken = lpToken0.token.symbol === token.token.symbol ? lpToken1 : lpToken0
 
-    const getLiquidityTokenInfo = async ({uniPair, token0, token0Ref, token0Input, token1, token1Input, token1Ref}) => {
-        if (!!uniPair) {
-            const total = await totalSupply(uniPair)
-            const totalTokenAmount = await new TokenAmount(uniPair.liquidityToken, total)
-            const token0Amount = await new TokenAmount(uniPair?.[token0Ref?.current?.name], BigInt(Math.round(token0Input * (10 ** token0?.token?.decimals))))
-            const token1Amount = await new TokenAmount(uniPair?.[token1Ref?.current?.name], BigInt(Math.round(token1Input * (10 ** token1?.token?.decimals))))
-            const uniswapTokensMinted = uniPair?.getLiquidityMinted(totalTokenAmount, token0Amount, token1Amount).toFixed(uniPair.liquidityToken.decimals)
-            const percentageOfPool = uniswapTokensMinted / totalTokenAmount.toFixed(uniPair.liquidityToken.decimals)
+        const route = new Route([pair], uniswapTokens[token.token.symbol])
+        const midPrice = route.midPrice.toSignificant(6)
+
+        const token1 = Object.entries(uniswapTokens).filter(item => item[0] !== token.token.symbol)[0][1]
+        const token1Input = token0Input * midPrice
+
+        if (token?.fiatBalance > pairToken?.fiatBalance) {
+            setMaxError(`insufficient ${pairToken?.token?.symbol} balance`)
+            setState(state => ({...state, [token.token.symbol]: 0}))
+            setState(state => ({...state, [token1.symbol]: 0}))
+            setLiquidityInfo({})
+        } else {
+            setState(state => ({...state, [token.token.symbol]: token0Input}))
+            setState(state => ({...state, [token1.symbol]: token1Input}))
+            setMaxError("")
+
+            const liquidityInfo = await getLiquidityPairInfo({
+                pair: pair,
+                token0: token0,
+                token0Input: token0Input,
+                token1: token1,
+                token1Input: token1Input
+            })
+            setLiquidityInfo(liquidityInfo)
+        }
+    }
+    const getLiquidityPairInfo = async ({pair, token0, token0Input, token1, token1Input}) => {
+        if (!!pair) {
+            const total = await totalPairSupply(pair)
+            const totalTokenAmount = await new TokenAmount(pair.liquidityToken, total)
+            const token0Amount = await new TokenAmount(token0, BigInt(Math.round(token0Input * (10 ** token0?.decimals))))
+            const token1Amount = await new TokenAmount(token1, BigInt(Math.round(token1Input * (10 ** token1?.decimals))))
+            const uniswapTokensMinted = pair?.getLiquidityMinted(totalTokenAmount, token0Amount, token1Amount).toFixed(pair.liquidityToken.decimals)
+            const percentageOfPool = uniswapTokensMinted / totalTokenAmount.toFixed(pair.liquidityToken.decimals)
 
             return {uniswapTokensMinted, percentageOfPool, total: formatUnits(BigNumber.from(total._hex))}
         }
     }
 
-
-    const [liquidityInfo, setLiquidityInfo] = useState({})
-    const [maxError, setMaxError] = useState("")
-    const setMax = async (clickedToken, clickedTokenRef) => {
-        const clickedTokenName = clickedTokenRef?.current?.name
-        const clickedTokenMax = clickedTokenRef?.current?.max
-        const clickedTokenBalance = Number((clickedToken?.balance / 10 ** clickedToken?.token?.decimals))
-        const pairToken = clickedTokenName === "token0" ? lpToken1 : lpToken0
-        const pairTokenRef = clickedTokenName === "token0" ? token1InputRef : token0InputRef
-        const pairTokenName = pairTokenRef?.current?.name
-        const route = new Route([globalPair], selectedTokens[clickedTokenName])
-        const midPrice = route.midPrice.toSignificant(6)
-
-
-        if (clickedToken?.fiatBalance > pairToken?.fiatBalance) {
-            setMaxError(`insufficient ${pairToken?.token?.symbol} balance`)
-            setState(state => ({...state, [clickedTokenName]: 0}))
-            setState(state => ({...state, [pairTokenName]: 0}))
-            setLiquidityInfo({})
-        } else {
-            const pairTokenInput = clickedTokenBalance * midPrice
-            setState(state => ({...state, [clickedTokenName]: clickedTokenMax}))
-            setState(state => ({...state, [pairTokenName]: pairTokenInput}))
-            setMaxError("")
-
-            const liquidityInfo = await getLiquidityTokenInfo({
-                uniPair: globalPair,
-                token0: clickedToken,
-                token0Ref: clickedTokenRef,
-                token0Input: clickedTokenMax,
-                token1: pairToken,
-                token1Ref: pairTokenRef,
-                token1Input: pairTokenInput
-            })
-            setLiquidityInfo(liquidityInfo)
-        }
+    const init = async () => {
+        setState(state => ({...state, [lpToken0.token.symbol]: 0}))
+        setState(state => ({...state, [lpToken1.token.symbol]: 0}))
+        const uniPair = await Fetcher.fetchPairData(uniswapTokens[lpToken0.token.symbol], uniswapTokens[lpToken1.token.symbol])
+        await setPair(uniPair)
     }
 
+    useEffect(() => {
+        init()
+
+    }, [])
 
     return (
         <Modal close={closeUniswapLpModal} heading={"Uniswap LP"}>
@@ -200,18 +168,18 @@ const UniswapLpModal = ({safeAddress}) => {
                 <div className="flex w-full flex-col rounded-xl bg-slate-100 dark:bg-slate-800">
                     <div className="flex flex-row">
                         <input
-                            value={state?.token0 || (readableBalance(lpToken0) < .1 ? 0 : '')}
-                            onChange={(e) => handleSetValue(e, lpToken0, token0InputRef)}
+                            value={state?.[lpToken0.token.symbol] || (readableTokenBalance(lpToken0) < .1 ? 0 : '')}
+                            onChange={(e) => handleSetTokenValue(e, lpToken0, token0InputRef)}
                             className="h-16 w-full appearance-none rounded-lg bg-slate-100 py-2 px-3 text-xl leading-tight focus:outline-none dark:bg-slate-800"
                             id="name"
-                            name="token0"
+                            name={lpToken0?.token?.symbol}
                             type="number"
                             step={0.000001}
                             placeholder="0.0"
                             required
                             max={lpToken0?.balance / 10 ** lpToken0?.token?.decimals}
                             ref={token0InputRef}
-                            disabled={!globalPair}
+                            disabled={!pair}
                         />
                         {/* Button to select token  */}
                         <div className="m-2 w-2/12 rounded-xl bg-slate-200 p-2 text-center text-xl dark:bg-slate-700">
@@ -220,11 +188,11 @@ const UniswapLpModal = ({safeAddress}) => {
                     </div>
                     <div className="flex w-full flex-row justify-end space-x-2 px-2">
                         <div>balance:</div>
-                        <div>{readableBalance(lpToken0)}</div>
+                        <div>{readableTokenBalance(lpToken0)}</div>
                     </div>
                     <div
-                        className={`mr-3 flex cursor-pointer justify-end text-[#FC8D4D] ${!globalPair ? 'pointer-events-none' : ''}`}
-                        onClick={() => setMax(lpToken0, token0InputRef)}
+                        className={`mr-3 flex cursor-pointer justify-end text-[#FC8D4D] ${!pair ? 'pointer-events-none' : ''}`}
+                        onClick={() => handleSetMaxTokenValue(lpToken0, token0InputRef)}
                     >
                         max
                     </div>
@@ -233,18 +201,18 @@ const UniswapLpModal = ({safeAddress}) => {
                 <div className="flex w-full flex-col rounded-xl bg-slate-100 dark:bg-slate-800">
                     <div className="flex flex-row">
                         <input
-                            value={state?.token1 || (readableBalance(lpToken1) < .1 ? 0 : '')}
-                            onChange={(e) => handleSetValue(e, lpToken1, token1InputRef)}
+                            value={state?.[lpToken1.token.symbol] || (readableTokenBalance(lpToken1) < .1 ? 0 : '')}
+                            onChange={(e) => handleSetTokenValue(e, lpToken1, token1InputRef)}
                             className="h-16 w-full appearance-none rounded-lg bg-slate-100 py-2 px-3 text-xl leading-tight focus:outline-none dark:bg-slate-800"
                             id="name"
-                            name="token1"
+                            name={lpToken1?.token?.symbol}
                             type="number"
                             step={0.000001}
                             placeholder="0.0"
                             required
                             max={lpToken1?.balance / 10 ** lpToken1?.token?.decimals}
                             ref={token1InputRef}
-                            disabled={!globalPair}
+                            disabled={!pair}
                         />
                         {/* Button to select token  */}
                         <div className="m-2 w-2/12 rounded-xl bg-slate-200 p-2 text-center text-xl dark:bg-slate-700">
@@ -253,11 +221,11 @@ const UniswapLpModal = ({safeAddress}) => {
                     </div>
                     <div className="flex w-full flex-row justify-end space-x-2 px-2">
                         <div>balance:</div>
-                        <div>{readableBalance(lpToken1)}</div>
+                        <div>{readableTokenBalance(lpToken1)}</div>
                     </div>
                     <div
-                        className={`mr-3 flex cursor-pointer justify-end text-[#FC8D4D] ${!globalPair ? 'pointer-events-none' : ''}`}
-                        onClick={() => setMax(lpToken1, token1InputRef)}
+                        className={`mr-3 flex cursor-pointer justify-end text-[#FC8D4D] ${!pair ? 'pointer-events-none' : ''}`}
+                        onClick={() => handleSetMaxTokenValue(lpToken1, token1InputRef)}
                     >
                         max
                     </div>
