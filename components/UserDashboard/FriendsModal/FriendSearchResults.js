7a5bc90a1037a2ea0a94ab46ad9bde0278a8db83
Jordan Marsaw <jordanmarsaw@Jordans-MBP.lan>
2022-04-20 13:56:01 -0400

    making sure we distinguish between frens and follows in search results

--- components/UserDashboard/FriendsModal/FriendSearchResults.js
+++ components/UserDashboard/FriendsModal/FriendSearchResults.js
@@ -1,10 +1,17 @@
 import React from "react"
 import FriendSearchCard from "./FriendSearchCard"
 
-const FriendSearchResults = ({ friends, closeModal }) => {
+const FriendSearchResults = ({ friends, closeModal, follows }) => {
+  console.log('frens ', friends)
   return (
     <div className="mt-3 flex h-full flex-col overflow-scroll">
+      <h1>frens</h1>
       {friends.map((friend, index) => (
+        <FriendSearchCard key={index} friend={friend} closeModal={closeModal} />
+      ))}
+      <br />
+      <h1>{ follows ? 'follows': ''}</h1>
+      {follows.map((friend, index) => (
         <FriendSearchCard key={index} friend={friend} closeModal={closeModal} />
       ))}
     </div>
