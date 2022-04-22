8645b12f06f258736f06da2f6795b5ffd3130d6f
joe lazarz <jlazarz87@gmail.com>
2022-04-22 15:16:49 -0400

    10members and addressEnsloading

--- components/Playground/UserDaos/DaoCard/DaoMembers.js
+++ components/Playground/UserDaos/DaoCard/DaoMembers.js
@@ -2,9 +2,8 @@ import React from "react"
 import DaoMemberBtn from "./DaoMemberBtn"
 
 const DaoMembers = ({ owners }) => {
-  // only display first 8-10 owners, rest can be hidden in DaoCardExpanded/info
-
-  const daoMembers = owners?.map((member, index) => <DaoMemberBtn key={index} member={member} />)
+  
+  const daoMembers = owners?.slice(0, 10).map((member, index) => <DaoMemberBtn key={index} member={member} />)
 
   return <div className="flex flex-row flex-wrap h-fit lg:w-2/3 w-full">{daoMembers}</div>
 }
--- components/Playground/UserPanel/AddressEns.js
+++ components/Playground/UserPanel/AddressEns.js
@@ -8,15 +8,19 @@ const AddressEns = ({ address }) => {
   })
 
   if (ensLoading || !address) {
-    return <div className="flex w-full justify-center animate-pulse">loading</div>
+    return (
+      <div className="flex h-10 w-full animate-pulse justify-center rounded-xl bg-slate-200 dark:bg-slate-800"></div>
+    )
   }
 
   return (
-    <div className="flex flex-row space-x-1 items-center justify-center w-full">
-      <div className="text-xl">{ensData ? ensData : address.substring(0, 6) + "..." + address.substring(address.length - 4)}</div>
-      <button className="flex flex-row space-x-1 bg-slate-200 hover:border-teal-300 dark:bg-slate-800 dark:hover:bg-slate-700 border dark:border-slate-800 dark:hover:border-teal-300 text-xs p-2 rounded-full">
+    <div className="flex h-10 w-full flex-row items-center justify-center space-x-1">
+      <div className={ensData && ensData.length > 18 ? "text-md" : "text-xl"}>
+        {ensData ? ensData : address.substring(0, 6) + "..." + address.substring(address.length - 4)}
+      </div>
+      <button className="flex flex-row space-x-1 rounded-full border bg-slate-200 p-2 text-xs hover:border-teal-300 dark:border-slate-800 dark:bg-slate-800 dark:hover:border-teal-300 dark:hover:bg-slate-700">
         <BiCopy size={16} />
-        </button>
+      </button>
     </div>
   )
 }
