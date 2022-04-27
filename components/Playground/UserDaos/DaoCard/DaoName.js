16e34430c2c6e91d7ac333af11c5f8ac387fdf57
Théo Mode <milesxbridges@gmail.com>
2022-04-27 15:21:08 -0400

    text selectable on edit button

--- components/Playground/UserDaos/DaoCard/DaoName.js
+++ components/Playground/UserDaos/DaoCard/DaoName.js
@@ -1,17 +1,60 @@
-import React from "react"
-import { HiPencilAlt } from "react-icons/hi"
+import React, {useState}            from "react"
+import {HiCheckCircle, HiPencilAlt} from "react-icons/hi"
+import {walletSnippet}              from 'utils/helpers'
+import useForm         from '../../../../hooks/useForm'
+import Modal           from '../../../Layout/Modal'
 
-const DaoName = ({ safe, isMember }) => {
-  return (
-    <div className="flex flex-row items-center space-x-3">
-      <div className="text-2xl">{safe?.substring(0, 6) + "..." + safe.substring(safe.length - 4)}</div>
-      {isMember ? (
-        <button className="flex flex-col items-center justify-center w-full text-center text-sm border hover:text-teal-300 hover:border-white dark:hover:border-white border-slate-100 dark:border-slate-600 bg-slate-100 hover:bg-slate-200 dark:bg-slate-600 dark:hover:bg-slate-500 rounded-lg p-1">
-          <HiPencilAlt size={18} />
-        </button>
-      ) : null}
-    </div>
-  )
+const DaoName = ({safe, isMember}) => {
+    const {state, setState, handleChange} = useForm()
+
+    const handleFocus = (e) => {
+        e.target.select();
+    }
+
+    const handleSubmit = (e) => {
+        console.log('e', e)
+    }
+    const [isEditable, setIsEditable] = useState(false)
+
+    console.log('state', state.name)
+
+
+    return (
+        <div className="flex flex-row items-center space-x-3">
+            <div className="text-2xl">
+                <form
+                    className="flex w-full flex-col space-y-8 py-4"
+                    onSubmit={(e) => handleSubmit(e)}
+                >
+                    <input
+                        value={state?.name || walletSnippet(safe)}
+                        onChange={handleChange}
+                        onFocus={handleFocus}
+                        name={"name"}
+                        className={`focus:outline-0 bg-transparent ${isEditable ? 'border-b' : ''}`}
+                        disabled={!isEditable}
+                    />
+                </form>
+                {/*{walletSnippet(safe)}*/}
+            </div>
+            {isMember ? (
+                <button
+                    onClick={(e) => {
+                       // this.select()
+                        console.log('e', e)
+                        setIsEditable(flag => !flag)
+                        if(isEditable) {
+                            console.log('are editing')
+                        }
+                    }}
+                    className="flex flex-col items-center justify-center w-full text-center text-sm border hover:text-teal-300 hover:border-white dark:hover:border-white border-slate-100 dark:border-slate-600 bg-slate-100 hover:bg-slate-200 dark:bg-slate-600 dark:hover:bg-slate-500 rounded-lg p-1">
+                    {isEditable ?  <HiCheckCircle size={18} /> : <HiPencilAlt size={18}/>}
+
+
+                </button>
+            ) : null}
+        </div>
+    )
 }
 
 export default DaoName
--- components/Playground/index.js
+++ components/Playground/index.js
@@ -1,40 +1,40 @@
-import React from "react"
-import UserPanel from "./UserPanel"
-import Feed from "./Feed"
-import UserDaos from "./UserDaos"
-import { useAccount, useEnsLookup } from "wagmi"
-import * as api from "../../query"
-import { useMutation } from "react-query"
+import {useEffect}                from 'react'
+import {useMutation}              from "react-query"
+import {useAccount, useEnsLookup} from "wagmi"
+import * as api                   from "../../query"
+import Feed                       from "./Feed"
+import UserDaos                   from "./UserDaos"
+import UserPanel                  from "./UserPanel"
 
-const Playground = ({ address, data }) => {
-  // data is the res from querying gnosis for the user's daos
-  // address is the address of the profile being viewed
-  // data:userData is the data of the signed-in user
-  const [{ data: userData, error: userErr, loading: userLoading }] = useAccount()
-  const [{ data: ensData, error: ensError, loading: ensLoading }] = useEnsLookup({ address: address })
+const Playground = ({address, data}) => {
+    // data is the res from querying gnosis for the user's daos
+    // address is the address of the profile being viewed
+    // data:userData is the data of the signed-in user
+    const [{data: userData, error: userErr, loading: userLoading}] = useAccount()
+    const [{data: ensData, error: ensError, loading: ensLoading}] = useEnsLookup({address: address})
 
-  const { status, mutateAsync: getUser } = useMutation(api.getUser)
-  React.useEffect(() => {
-    if (ensLoading) {
-      return
-    }
-    const req = { address: address, ens: ensData }
-    // see if we can adjust our getUser function to update ens
-    getUser(req)
-  }, [ensLoading])
+    const {status, mutateAsync: getUser} = useMutation(api.getUser)
+    useEffect(() => {
+        if (ensLoading) {
+            return
+        }
+        const req = {address: address, ens: ensData}
+        // see if we can adjust our getUser function to update ens
+        getUser(req)
+    }, [ensLoading])
 
-  // i think there's an opportunity to toggle UserDaos out for another set of components
-  // Feed and UserDaos(or a component that contains UserDaos / toggles it out) are intended
-  // to interact with each other, actions taken in both sections should lead to discovery
-  // and exploration of the app - making each column or section modular could provide unique
-  // user paths and experiences - bbyDAO and user discovery/connection
-  return (
-    <div className="flex w-full flex-col lg:flex-row">
-      <UserPanel user={userData?.address} address={address} />
-      <UserDaos user={userData?.address} data={data} />
-      <Feed />
-    </div>
-  )
+    // i think there's an opportunity to toggle UserDaos out for another set of components
+    // Feed and UserDaos(or a component that contains UserDaos / toggles it out) are intended
+    // to interact with each other, actions taken in both sections should lead to discovery
+    // and exploration of the app - making each column or section modular could provide unique
+    // user paths and experiences - bbyDAO and user discovery/connection
+    return (
+        <div className="flex w-full flex-col lg:flex-row">
+            <UserPanel user={userData?.address} address={address}/>
+            <UserDaos user={userData?.address} data={data}/>
+            <Feed/>
+        </div>
+    )
 }
 
 export default Playground
