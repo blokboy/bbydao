1c3620a9650e275476b808c07ec25e263f016a22
Théo Mode <milesxbridges@gmail.com>
2022-04-27 15:40:14 -0400

    mutate state

--- components/Playground/UserDaos/DaoCard/DaoName.js
+++ components/Playground/UserDaos/DaoCard/DaoName.js
@@ -1,33 +1,39 @@
-import React, {useState}            from "react"
-import {HiCheckCircle, HiPencilAlt} from "react-icons/hi"
+import useForm                      from 'hooks/useForm'
+import * as api                     from 'query'
+import React, {useState}            from 'react'
+import {HiCheckCircle, HiPencilAlt} from 'react-icons/hi'
+import {useMutation, useQuery}      from 'react-query'
 import {walletSnippet}              from 'utils/helpers'
-import useForm         from '../../../../hooks/useForm'
-import Modal           from '../../../Layout/Modal'
 
 const DaoName = ({safe, isMember}) => {
-    const {state, setState, handleChange} = useForm()
-
+    const [isEditable, setIsEditable] = useState(false)
+    const {state, handleChange} = useForm()
+    const {mutateAsync} = useMutation(api.updateDao, {
+        refetchActive: true,
+    })
+    const {data} = useQuery(
+        [`${safe}`],
+        () => api.getDao({address: safe}),
+        {staleTime: 180000}
+    )
     const handleFocus = (e) => {
         e.target.select();
     }
-
-    const handleSubmit = (e) => {
-        console.log('e', e)
+    const handleSave = (e) => {
+        setIsEditable(flag => !flag)
+        if (isEditable) {
+            mutateAsync({id: data.id, name: state.name})
+        }
     }
-    const [isEditable, setIsEditable] = useState(false)
-
-    console.log('state', state.name)
-
 
     return (
         <div className="flex flex-row items-center space-x-3">
             <div className="text-2xl">
                 <form
                     className="flex w-full flex-col space-y-8 py-4"
-                    onSubmit={(e) => handleSubmit(e)}
                 >
                     <input
-                        value={state?.name || walletSnippet(safe)}
+                        value={state?.name || data?.name || walletSnippet(safe)}
                         onChange={handleChange}
                         onFocus={handleFocus}
                         name={"name"}
@@ -35,22 +41,12 @@ const DaoName = ({safe, isMember}) => {
                         disabled={!isEditable}
                     />
                 </form>
-                {/*{walletSnippet(safe)}*/}
             </div>
             {isMember ? (
                 <button
-                    onClick={(e) => {
-                       // this.select()
-                        console.log('e', e)
-                        setIsEditable(flag => !flag)
-                        if(isEditable) {
-                            console.log('are editing')
-                        }
-                    }}
+                    onClick={(e) => handleSave(e)}
                     className="flex flex-col items-center justify-center w-full text-center text-sm border hover:text-teal-300 hover:border-white dark:hover:border-white border-slate-100 dark:border-slate-600 bg-slate-100 hover:bg-slate-200 dark:bg-slate-600 dark:hover:bg-slate-500 rounded-lg p-1">
-                    {isEditable ?  <HiCheckCircle size={18} /> : <HiPencilAlt size={18}/>}
-
-
+                    {isEditable ? <HiCheckCircle size={18}/> : <HiPencilAlt size={18}/>}
                 </button>
             ) : null}
         </div>
