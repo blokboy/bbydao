97a441f28880830935709cb4044d2a436ba171dd
joe lazarz <jlazarz87@gmail.com>
2022-05-02 12:48:15 -0400

    unfollow dropdown

--- components/Playground/UserPanel/UserFollow/FollowUserBtn.js
+++ components/Playground/UserPanel/UserFollow/FollowUserBtn.js
@@ -1,15 +1,15 @@
 import React from "react"
-import { BiUserPlus } from "react-icons/bi"
-import { BiUserCheck } from "react-icons/bi"
+import { BiUserPlus, BiUserCheck, BiUserX } from "react-icons/bi"
 import * as api from "/query"
 import { useMutation, useQueryClient } from "react-query"
+import ClickAwayListener from "react-click-away-listener"
 
 const FollowUserBtn = ({ user, address, friendStatus }) => {
   // request follow relationship with reqRelationship
   // follow enum is 4
   // once request is sent (onSuccess), invalidate cached data and refetch
   const queryClient = useQueryClient()
-  const {status, mutateAsync: follow} = useMutation(api.reqRelationship, {
+  const { status, mutateAsync: follow } = useMutation(api.reqRelationship, {
     onSuccess: async () => {
       await queryClient.invalidateQueries(["friends", address], {
         refetchActive: true,
@@ -26,7 +26,7 @@ const FollowUserBtn = ({ user, address, friendStatus }) => {
     follow(req)
   }
 
-  const {mutateAsync: unfollow} = useMutation(api.updateRelationship, {
+  const { mutateAsync: unfollow } = useMutation(api.updateRelationship, {
     onSuccess: async () => {
       await queryClient.invalidateQueries(["friends", address], {
         refetchActive: true,
@@ -44,22 +44,53 @@ const FollowUserBtn = ({ user, address, friendStatus }) => {
     unfollow(req)
   }
 
+  const [unfollowDisplay, setUnfollowDisplay] = React.useState(false)
+
+  const unfollowDisplayCallback = React.useCallback(() => {
+    setUnfollowDisplay(true)
+  }, [])
+
+  const handleClickAway = React.useCallback(() => {
+    if (unfollowDisplay) {
+      setUnfollowDisplay(false)
+    }
+  }, [unfollowDisplay])
+
+  const unfollowDropdown = React.useMemo(() => {
+    return unfollowDisplay ? (
+      <div className="absolute w-36 translate-y-2 rounded-lg border border-red-600 bg-slate-200 p-1.5 text-xs text-red-600 dark:border-red-300 dark:bg-slate-800 dark:text-red-300">
+        <button
+          className="flex flex-row space-x-1 rounded-lg border bg-slate-200 p-2 text-xs hover:border-red-300 dark:border-slate-800 dark:bg-slate-800 dark:hover:border-red-300 dark:hover:bg-slate-700"
+          onClick={handleUnfollow}
+        >
+          <BiUserX size={16} />
+          Unfollow
+        </button>
+      </div>
+    ) : null
+  }, [unfollowDisplay])
+
   if (friendStatus?.isFollowing) {
     return (
-      <button 
-      className="flex w-36 flex-row items-center justify-center space-x-3 rounded-lg border border-slate-200 bg-slate-200 p-1 dark:border-slate-800 dark:bg-slate-800 hover:dark:bg-slate-700 hover:dark:border-red-300 hover:border-red-300"
-      onClick={handleUnfollow}
-      >
-        <BiUserCheck size={18} />
-        <span>unfollow</span>
-      </button>
+      <ClickAwayListener onClickAway={handleClickAway}>
+        <div>
+          <button
+            className="flex w-36 flex-row items-center justify-center space-x-3 rounded-lg border border-slate-200 bg-slate-200 p-1 hover:border-red-300 dark:border-slate-800 dark:bg-slate-800 hover:dark:border-red-300 hover:dark:bg-slate-700"
+            onClick={unfollowDisplayCallback}
+          >
+            <BiUserCheck size={18} />
+            <span>following</span>
+          </button>
+          {unfollowDropdown}
+        </div>
+      </ClickAwayListener>
     )
   }
 
   return (
-    <button 
-    className="flex w-36 flex-row items-center justify-center space-x-3 rounded-lg border bg-slate-200 p-1 hover:border-teal-300 dark:border-slate-800 dark:bg-slate-800 dark:hover:border-teal-300 dark:hover:bg-slate-700"
-    onClick={handleFollow}
+    <button
+      className="flex w-36 flex-row items-center justify-center space-x-3 rounded-lg border bg-slate-200 p-1 hover:border-teal-300 dark:border-slate-800 dark:bg-slate-800 dark:hover:border-teal-300 dark:hover:bg-slate-700"
+      onClick={handleFollow}
     >
       <BiUserPlus size={18} />
       <span>follow</span>
