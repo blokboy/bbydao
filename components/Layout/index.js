e63d7b67a62ffb08991d27655d9078ebf35a6a82
joe lazarz <jlazarz87@gmail.com>
2022-05-10 14:03:33 -0400

    cleaning up layout and add formik

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,10 +3,7 @@ import dynamic from 'next/dynamic'
 const Search = dynamic(() => import('./Search'))
 import { useLayoutStore } from "/stores/useLayoutStore"
 import ConnectModal from "./ConnectModal"
-import AppSearchModal from "../AppSearchModal"
 import CreateThreadModal from "../Messages/CreateThreadModal"
-import FriendsModal from "../UserDashboard/FriendsModal"
-import UnfriendModal from "../UserDashboard/UnfriendModal"
 import OfferModal from "../OpenSeaCollection/OfferModal"
 import BuyModal from "../OpenSeaCollection/BuyModal"
 import NetworkWarning from "./Nav/NetworkWarning"
@@ -21,12 +18,9 @@ const Layout = ({ children }) => {
       <NetworkWarning />
       {searchOpen ? <Search /> : children}
       <ConnectModal />
-      <AppSearchModal />
       <CreateThreadModal />
-      <FriendsModal />
       <OfferModal />
       <BuyModal />
-      <UnfriendModal />
       <MobileNotificationsModal />
     </main>
   )
