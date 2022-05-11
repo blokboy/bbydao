36eb6d84141f9644aa177a39630e5cb767e9b1fb
Théo Mode <milesxbridges@gmail.com>
2022-05-11 10:06:26 -0400

    tokenAmounts in LP

--- components/Dao/UniswapLpModal/RemoveLiquidity.js
+++ components/Dao/UniswapLpModal/RemoveLiquidity.js
@@ -1,15 +1,15 @@
-import {ChainId, Fetcher, Token} from '@uniswap/sdk'
-import {BigNumber, ethers}       from "ethers"
-import React, { useCallback, useEffect, useState } from "react"
-import { useQueryClient }                          from "react-query"
-import { useSigner }                               from "wagmi"
-import useForm                                     from "hooks/useForm"
-import IUniswapV2Pair                              from "@uniswap/v2-periphery/build/IUniswapV2Pair.json"
-import UniswapV2ERC20                              from "@uniswap/v2-core/build/UniswapV2ERC20.json"
+import { ChainId, Fetcher, Token, TokenAmount } from "@uniswap/sdk"
+import { BigNumber, ethers } from "ethers"
+import React, { useEffect, useState } from "react"
+import { useQueryClient } from "react-query"
+import { useSigner } from "wagmi"
+import useForm from "hooks/useForm"
+import IUniswapV2Pair from "@uniswap/v2-periphery/build/IUniswapV2Pair.json"
+import UniswapV2ERC20 from "@uniswap/v2-core/build/UniswapV2ERC20.json"
 
-import {max256, NumberFromBig}                     from 'utils/helpers'
-import {minimalABI}                                from '../../../hooks/useERC20Contract'
-import {amount, handleGnosisTransaction}           from './helpers'
+import { max256, NumberFromBig } from "utils/helpers"
+import { minimalABI } from "../../../hooks/useERC20Contract"
+import { amount, handleGnosisTransaction } from "./helpers"
 
 const RemoveLiquidity = ({ token }) => {
   const UniswapV2Router02 = ethers.utils.getAddress("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
@@ -22,7 +22,7 @@ const RemoveLiquidity = ({ token }) => {
   const [breakDown, setBreakDown] = useState({})
   const pairName = token?.token?.name.replace("Uniswap V2", "").replace("Pool", "")
 
-   React.useMemo(async () => {
+  React.useMemo(async () => {
     if (!!signer) {
       const pairContract = new ethers.Contract(ethers.utils.getAddress(tokenAddress), IUniswapV2Pair["abi"], signer)
       const pairERC20Contract = new ethers.Contract(
@@ -31,101 +31,100 @@ const RemoveLiquidity = ({ token }) => {
         signer
       )
 
-
-      // console.log('contract', pairContract)
-      // console.log('contract 2', pairERC20Contract)
-
       const totalSupply = await pairContract?.totalSupply()
       const bbyDaoBalance = await pairERC20Contract?.balanceOf(bbyDao)
-      const percentageOfPool =  NumberFromBig(bbyDaoBalance, pairToken?.decimals) / NumberFromBig(totalSupply, pairToken?.decimals)
+      const percentageOfPool =
+        NumberFromBig(bbyDaoBalance, pairToken?.decimals) / NumberFromBig(totalSupply, pairToken?.decimals)
       const bbyDaoAllowance = await pairERC20Contract?.allowance(bbyDao, UniswapV2Router02)
-
+      const kLast = await pairContract?.kLast()
 
       const reserves = await pairContract?.getReserves()
-      const token0Contract =  new ethers.Contract(ethers.utils.getAddress(await pairContract?.token0()), minimalABI, signer)
-      const token1Contract =  new ethers.Contract(ethers.utils.getAddress(await pairContract?.token1()), minimalABI, signer)
-
-
+      const token0Contract = new ethers.Contract(
+        ethers.utils.getAddress(await pairContract?.token0()),
+        minimalABI,
+        signer
+      )
+      const token1Contract = new ethers.Contract(
+        ethers.utils.getAddress(await pairContract?.token1()),
+        minimalABI,
+        signer
+      )
 
       const token0 = {
-        address:  await pairContract?.token0(),
+        address: await pairContract?.token0(),
         decimals: await token0Contract?.decimals(),
         symbol: await token0Contract?.symbol(),
         name: await token0Contract?.name(),
-        reserves: reserves[0]
+        reserves: reserves[0],
       }
 
       const token1 = {
-        address:  await pairContract?.token1(),
+        address: await pairContract?.token1(),
         decimals: await token1Contract?.decimals(),
         symbol: await token1Contract?.symbol(),
         name: await token1Contract?.name(),
-        reserves: reserves[1]
+        reserves: reserves[1],
       }
-
-      const priceOfToken1InToken0 = Number((Number(token0.reserves) * token0.decimals) / (Number(token1.reserves) * token1.decimals))
-      const priceOfToken0InToken1 = Number((Number(token1.reserves) * token1.decimals) / (Number(token0.reserves) * token0.decimals))
-
-      const factory = await pairContract?.factory()
-
-
-       console.log('to', Number(totalSupply / (10 ** pairToken?.decimals)))
+      const priceOfToken1InToken0 = Number(
+        (Number(token0.reserves) * token0.decimals) / (Number(token1.reserves) * token1.decimals)
+      )
+      const priceOfToken0InToken1 = Number(
+        (Number(token1.reserves) * token1.decimals) / (Number(token0.reserves) * token0.decimals)
+      )
 
       const uniswapTokens = async () => {
-        const uniToken0 = new Token(
-            ChainId.MAINNET,
-            token0?.address,
-            token0?.decimals,
-            token0?.symbol,
-            token0?.name
-        )
-
-        const uniToken1 = new Token(
-            ChainId.MAINNET,
-            token1?.address,
-            token1?.decimals,
-            token1?.symbol,
-            token1?.name
-        )
-
+        const uniToken0 = new Token(ChainId.MAINNET, token0?.address, token0?.decimals, token0?.symbol, token0?.name)
+        const uniToken1 = new Token(ChainId.MAINNET, token1?.address, token1?.decimals, token1?.symbol, token1?.name)
         return { [token0.symbol]: uniToken0, [token1.symbol]: uniToken1 }
       }
       const uniTokens = await uniswapTokens()
-      const uniPair = await Fetcher.fetchPairData(
-          uniTokens[token0.symbol],
-          uniTokens[token1.symbol]
-      )
-
-      // console.log('uniPair', uniPair)
-
-      console.log('uniPair', uniPair?.liquidityToken, totalSupply, bbyDaoBalance)
-
-      console.log('m', uniPair?.getLiquidityValue(uniPair?.liquidityToken, totalSupply, bbyDaoBalance))
-
+      const uniPair = await Fetcher.fetchPairData(uniTokens[token0.symbol], uniTokens[token1.symbol])
+      const totalSupplyTokenAmount = new TokenAmount(uniPair?.liquidityToken, totalSupply)
+      const bbyDaoLiquidityTokenAmount = new TokenAmount(uniPair?.liquidityToken, bbyDaoBalance)
+      const token0Amount = uniPair
+        ?.getLiquidityValue(
+          uniPair.tokenAmounts[0].token,
+          totalSupplyTokenAmount,
+          bbyDaoLiquidityTokenAmount,
+          false,
+          kLast
+        )
+        .toFixed(pairToken.decimals)
+      const token1Amount = uniPair
+        ?.getLiquidityValue(
+          uniPair.tokenAmounts[1].token,
+          totalSupplyTokenAmount,
+          bbyDaoLiquidityTokenAmount,
+          false,
+          kLast
+        )
+        .toFixed(pairToken.decimals)
 
       setBreakDown({
         ...breakDown,
         poolTokens: NumberFromBig(bbyDaoBalance, pairToken?.decimals),
         hasAllowance: parseFloat(amount(bbyDaoAllowance, pairToken?.decimals)) > 0,
         pairContract,
-        percentageOfPool: `${((percentageOfPool * 100) < .01 ) ? '< 0.01' : parseFloat((percentageOfPool * 100).toString()).toFixed(6)}%`,
+        percentageOfPool: `${
+          percentageOfPool * 100 < 0.01 ? "< 0.01" : parseFloat((percentageOfPool * 100).toString()).toFixed(6)
+        }%`,
         tokens: {
           token0: {
             name: token0.name,
             symbol: token0.symbol,
-            priceInPair: priceOfToken0InToken1
+            priceInPair: priceOfToken0InToken1,
+            amount: token0Amount
           },
           token1: {
             name: token1.name,
             symbol: token1.symbol,
-            priceInPair: priceOfToken1InToken0
-          }
-        }
+            priceInPair: priceOfToken1InToken0,
+            amount: token1Amount
+          },
+        },
       })
-
     }
   }, [tokenAddress, signer, bbyDao])
-
 
   useEffect(() => {
     if (!liquidity) setState({ liquidity: 0 })
@@ -197,27 +196,36 @@ const RemoveLiquidity = ({ token }) => {
         </div>
         <div>Amount of Tokens You will Receive back</div>
         <div>
-          <div>Current Position</div>
-          <div>{pairName}</div>
-          <div>Your Total Pool Tokens: {breakDown?.poolTokens}</div>
-          <div>Your Pool Share: {breakDown?.percentageOfPool}</div>
           <div>
-            <div>Price</div>
-            <div>1 {breakDown?.tokens?.token0?.symbol} = {breakDown?.tokens?.token0?.priceInPair} {breakDown?.tokens?.token1?.symbol}</div>
-            <div>1 {breakDown?.tokens?.token1?.symbol} = {breakDown?.tokens?.token1?.priceInPair} {breakDown?.tokens?.token0?.symbol}</div>
+            <div>Your Position</div>
+            <div>{pairName}</div>
+            <div>Your Total Pool Tokens: {breakDown?.poolTokens}</div>
+            <div>Your Pool Share: {breakDown?.percentageOfPool}</div>
+            <div>{breakDown?.tokens?.token0?.symbol}: {breakDown?.tokens?.token0?.amount}</div>
+            <div>{breakDown?.tokens?.token1?.symbol}: {breakDown?.tokens?.token1?.amount}</div>
 
           </div>
-
+          <div>
+            <div>Price</div>
+            <div>
+              1 {breakDown?.tokens?.token0?.symbol} = {breakDown?.tokens?.token0?.priceInPair}{" "}
+              {breakDown?.tokens?.token1?.symbol}
+            </div>
+            <div>
+              1 {breakDown?.tokens?.token1?.symbol} = {breakDown?.tokens?.token1?.priceInPair}{" "}
+              {breakDown?.tokens?.token0?.symbol}
+            </div>
+          </div>
         </div>
       </div>
 
       {breakDown.hasAllowance === false && (
-          <div
-              className="flex cursor-pointer items-center justify-center rounded-3xl bg-[#FC8D4D] p-4 font-normal text-white hover:bg-[#d57239]"
-              onClick={() => handleApprovePair(breakDown?.pairContract)}
-          >
-            Approve {pairName} UNI-V2 LP Token
-          </div>
+        <div
+          className="flex cursor-pointer items-center justify-center rounded-3xl bg-[#FC8D4D] p-4 font-normal text-white hover:bg-[#d57239]"
+          onClick={() => handleApprovePair(breakDown?.pairContract)}
+        >
+          Approve {pairName} UNI-V2 LP Token
+        </div>
       )}
     </div>
   )
