8c213b62d7cec7e9fce85383c5c7703147a99768
joe lazarz <jlazarz87@gmail.com>
2022-05-12 12:16:27 -0400

    remove UserDashboard components

--- pages/user/[address]/index.js
+++ pages/user/[address]/index.js
@@ -1,44 +1,26 @@
 import axios from "axios"
-import UserDashboard from "components/UserDashboard/index"
-import SafeServiceClient from "@gnosis.pm/safe-service-client"
+import React from "react"
+import Playground from "/components/Playground"
 
-const UserPage = ({ data }) => {
-  return <UserDashboard data={data} />
+const index = ({ address, data }) => {
+  return <Playground address={address} data={data} />
 }
 
-export default UserPage
+export default index
 
 export const getServerSideProps = async ({ query }) => {
+  const address = query.address
   try {
-    const safeService = new SafeServiceClient(
-      "https://safe-transaction.gnosis.io"
-    )
-    const safes = await safeService.getSafesByOwner(query.address)
-
-    const res = await axios.post(
-      `${process.env.NEXT_PUBLIC_API}/accounts/account`,
-      {
-        address: query.address,
-      }
-    )
-    const user = res.data
-
-    if (safes.safes.length) {
-      const safeCreationInfo = await safeService.getSafeCreationInfo(
-        safes.safes[0]
-      )
-    
-      const safeBalanceInfo = {}
-      for(const safe of safes.safes) {
-        const balances = await safeService.getBalances(safe);
-        safeBalanceInfo[safe] = balances;
-      }
-
-      return { props: { data: { user, safes, safeCreationInfo, safeBalanceInfo } } }
+    const res = await axios.get(`https://safe-transaction.gnosis.io/api/v1/owners/${address}/safes/`)
+    const data = await res.data
+    return {
+      props: {
+        address,
+        data,
+      },
     }
-
-    return { props: { data: { user, safes } } }
   } catch (error) {
-    // return error
+    console.log(error)
+    return { props: {} }
   }
 }
