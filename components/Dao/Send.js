1a7af995deb1208379fd283fecd63681877e3b18
Théo Mode <milesxbridges@gmail.com>
2022-05-20 11:57:38 -0400

    init send modal

---
+++ components/Dao/Send.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const Send = ({ token }) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Send
--- components/Playground/UserDaos/DaoCard/DaoCardExpanded/DaoTokensTab/TokenControls.js
+++ components/Playground/UserDaos/DaoCard/DaoCardExpanded/DaoTokensTab/TokenControls.js
@@ -4,6 +4,7 @@ import RemoveLiquidity from "components/Dao/Uniswap/RemoveLiquidity"
 import Swap from "components/Dao/Uniswap/Swap"
 import Modal from "components/Layout/Modal"
 import { HiChevronDown, HiChevronUp } from "react-icons/hi"
+import Send from "../../../../../Dao/Send"
 
 const TokenControls = ({ liquidityPair, token, isUniV2, treasury }) => {
   const [toggleControls, setToggleControls] = React.useState(false)
@@ -15,124 +16,154 @@ const TokenControls = ({ liquidityPair, token, isUniV2, treasury }) => {
     )
   }, [treasury])
 
-  return (
-    <>
-      {!toggleControls && (
+  const ToggleButton = ({ icon }) => (
+    <button
+      type="button"
+      className="flex justify-center rounded bg-slate-200 shadow-xl dark:bg-slate-800"
+      onClick={() => setToggleControls(state => !state)}
+    >
+      {icon}
+    </button>
+  )
+
+  const SendButton = () => (
+    <Modal
+      heading={
+        <div className="flex items-center">
+          {/*<div className="h-8 w-8 overflow-hidden rounded-full" title="Uniswap V2 Swap">*/}
+          {/*    <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Uniswap_Logo.svg" />*/}
+          {/*</div>*/}
+          <div className="flex items-center text-xl font-normal">
+              Send {token?.symbol}
+              <div className="h-6 w-6 overflow-hidden rounded-full">
+                  {console.log('to', token)}
+                  <img src={token?.logoURI || token?.logoURI} />
+              </div>
+          </div>
+        </div>
+      }
+      trigger={
         <button
           type="button"
-          className="flex justify-center rounded bg-slate-200 shadow-xl dark:bg-slate-800"
-          onClick={() => setToggleControls(state => !state)}
+          className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
         >
-          <HiChevronDown size={24} />
+          Send
         </button>
-      )}
+      }
+    >
+      <Send token={token} />
+    </Modal>
+  )
+
+  const SwapButton = () => (
+    <Modal
+      heading={
+        <div className="flex items-center">
+          <div className="h-8 w-8 overflow-hidden rounded-full" title="Uniswap V2 Swap">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Uniswap_Logo.svg" />
+          </div>
+          <div className="flex items-center text-xl font-normal">
+            Swap <span className="ml-2 pt-[.1rem] text-xs">Uniswap (V2)</span>
+          </div>
+        </div>
+      }
+      trigger={
+        <button
+          type="button"
+          className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
+        >
+          Swap
+        </button>
+      }
+    >
+      <Swap token={token} />
+    </Modal>
+  )
+
+  const LPButton = () => (
+    <Modal
+      heading={
+        <div className="flex items-center">
+          <div className="h-8 w-8 overflow-hidden rounded-full" title="Uniswap V2 Swap">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Uniswap_Logo.svg" />
+          </div>
+          <div className="flex items-center text-xl font-normal">
+            Add Liquidity <span className="ml-2 pt-[.1rem] text-xs">Uniswap (V2)</span>
+          </div>
+        </div>
+      }
+      trigger={
+        <button
+          type="button"
+          className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
+        >
+          LP
+        </button>
+      }
+    >
+      <AddLiquidity lpToken0={token} />
+    </Modal>
+  )
+
+  const RemoveLiquidityButton = () => (
+    <Modal
+      heading={
+        <div className="flex items-center">
+          <div className="h-8 w-8 overflow-hidden rounded-full" title="Uniswap V2 Swap">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Uniswap_Logo.svg" />
+          </div>
+          <div className="flex items-center text-xl font-normal">
+            Remove Liquidity <span className="ml-2 pt-[.1rem] text-xs">Uniswap (V2)</span>
+          </div>
+        </div>
+      }
+      trigger={
+        <button
+          type="button"
+          className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
+        >
+          Remove Liquidity
+        </button>
+      }
+    >
+      <RemoveLiquidity token={token} />
+    </Modal>
+  )
+
+  const AddLiquidityButton = () => (
+    <Modal
+      heading={"Add Liquidity"}
+      trigger={
+        <button
+          type="button"
+          className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
+        >
+          Add Liquidity
+        </button>
+      }
+    >
+      <AddLiquidity lpToken0={liquidityPair?.lpToken0} token1={liquidityPair?.lpToken1} />
+    </Modal>
+  )
+
+  return (
+    <>
+      {!toggleControls && <ToggleButton icon={<HiChevronDown size={24} />} />}
       <div className={`${!toggleControls ? "h-0" : "h-auto"} overflow-hidden transition-all`}>
         {(!isUniV2 && (
           <div className="flex flex-wrap p-1 xl:justify-center">
-            <button
-              type="button"
-              className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
-            >
-              Send
-            </button>
-            <Modal
-              heading={
-                <div className="flex items-center">
-                  <div className="h-8 w-8 overflow-hidden rounded-full" title="Uniswap V2 Swap">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Uniswap_Logo.svg" />
-                  </div>
-                  <div className="flex items-center text-xl font-normal">
-                    Swap <span className="ml-2 pt-[.1rem] text-xs">Uniswap (V2)</span>
-                  </div>
-                </div>
-              }
-              trigger={
-                <button
-                  type="button"
-                  className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
-                >
-                  Swap
-                </button>
-              }
-            >
-              <Swap token={token} />
-            </Modal>
-            {hasTokensToLp && (
-              <Modal
-                heading={
-                  <div className="flex items-center">
-                    <div className="h-8 w-8 overflow-hidden rounded-full" title="Uniswap V2 Swap">
-                      <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Uniswap_Logo.svg" />
-                    </div>
-                    <div className="flex items-center text-xl font-normal">
-                      Add Liquidity <span className="ml-2 pt-[.1rem] text-xs">Uniswap (V2)</span>
-                    </div>
-                  </div>
-                }
-                trigger={
-                  <button
-                    type="button"
-                    className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
-                  >
-                    LP
-                  </button>
-                }
-              >
-                <AddLiquidity lpToken0={token} />
-              </Modal>
-            )}
+            <SendButton />
+            <SwapButton />
+            {hasTokensToLp && <LPButton />}
           </div>
         )) || (
           <div className="flex flex-wrap p-1 xl:justify-center">
-            <Modal
-              heading={
-                  <div className="flex items-center">
-                      <div className="h-8 w-8 overflow-hidden rounded-full" title="Uniswap V2 Swap">
-                          <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Uniswap_Logo.svg" />
-                      </div>
-                      <div className="flex items-center text-xl font-normal">
-                          Remove Liquidity <span className="ml-2 pt-[.1rem] text-xs">Uniswap (V2)</span>
-                      </div>
-                  </div>
-              }
-              trigger={
-                <button
-                  type="button"
-                  className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
-                >
-                  Remove Liquidity
-                </button>
-              }
-            >
-              <RemoveLiquidity token={token} />
-            </Modal>
-            {!!liquidityPair && (
-              <Modal
-                heading={"Add Liquidity"}
-                trigger={
-                  <button
-                    type="button"
-                    className={`mb-2 w-full rounded-lg bg-slate-300 p-2 text-sm text-white hover:bg-blue-600 hover:bg-blue-500 dark:bg-slate-700 hover:dark:bg-slate-800`}
-                  >
-                    Add Liquidity
-                  </button>
-                }
-              >
-                <AddLiquidity lpToken0={liquidityPair?.lpToken0} token1={liquidityPair?.lpToken1} />
-              </Modal>
-            )}
+            <RemoveLiquidityButton />
+            {!!liquidityPair && <AddLiquidityButton />}
           </div>
         )}
       </div>
-      {toggleControls && (
-        <button
-          type="button"
-          className="flex justify-center rounded bg-slate-200 shadow-xl dark:bg-slate-800"
-          onClick={() => setToggleControls(state => !state)}
-        >
-          <HiChevronUp size={24} />
-        </button>
-      )}
+      {toggleControls && <ToggleButton icon={<HiChevronUp size={24} />} />}
     </>
   )
 }
