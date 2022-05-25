c0e676e822a91631c810736d9023de8461e05b5d
Théo Mode <milesxbridges@gmail.com>
2022-05-25 12:59:17 -0400

    move tooltip

--- components/Dao/Uniswap/Slippage.js
+++ components/Dao/Uniswap/Slippage.js
@@ -4,20 +4,19 @@ import ToolTip from "../../Layout/Tooltip"
 const Slippage = ({ value, handleChange }) => {
   const [isOpen, setIsOpen] = React.useState(false)
   return (
-    <div>
+    <div className="flex mt-6">
       <div
         onClick={() => setIsOpen(bool => !bool)}
-        className={`mt-6 flex flex w-52 flex-col items-center rounded-xl py-2 text-sm font-thin hover:cursor-pointer hover:bg-slate-300 hover:dark:bg-slate-800 ${
+        className={`flex flex w-auto flex-col items-center rounded-xl p-2 text-sm font-thin hover:cursor-pointer hover:bg-slate-300 hover:dark:bg-slate-800 ${
           isOpen && "bg-slate-300 dark:bg-slate-800"
         }`}
       >
         <div className="flex flex items-center">
           <div>Slippage Tolerance</div>
-          <ToolTip>Your transaction will revert if the price changes unfavorably more than this percentage.</ToolTip>
           {value ? <div className="ml-2 text-xs italic">{value}%</div> : null}
         </div>
         {isOpen && (
-          <div className="my-2 flex h-8 w-3/4 items-center rounded-lg bg-slate-100 py-2 px-3 dark:bg-slate-700">
+          <div className="my-2 flex h-8 w-3/4 items-center rounded-lg bg-slate-100 px-3 dark:bg-slate-700">
             <input
               value={value}
               type="number"
@@ -30,6 +29,7 @@ const Slippage = ({ value, handleChange }) => {
           </div>
         )}
       </div>
+        <ToolTip>Your transaction will revert if the price changes unfavorably more than this percentage.</ToolTip>
     </div>
   )
 }
