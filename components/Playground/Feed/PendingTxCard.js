21cbef1815831565a186a46b1fa90d5925559ed4
Théo Mode <milesxbridges@gmail.com>
2022-06-07 00:30:11 -0400

    add rejection, improve styles pending

--- components/Playground/Feed/PendingTxCard.js
+++ components/Playground/Feed/PendingTxCard.js
@@ -1,14 +1,14 @@
-import SafeServiceClient    from '@gnosis.pm/safe-service-client'
-import Link                 from "next/link"
-import React                from "react"
-import { useRelativeTime }  from "hooks/useRelativeTime.ts"
-import { HiCheckCircle }    from "react-icons/hi"
-import { useLayoutStore }   from "../../../stores/useLayoutStore"
-import {usePlaygroundStore} from '../../../stores/usePlaygroundStore'
+import SafeServiceClient from "@gnosis.pm/safe-service-client"
+import React from "react"
+import { useRelativeTime } from "hooks/useRelativeTime.ts"
+import { HiCheckCircle } from "react-icons/hi"
+import useSafeSdk from "../../../hooks/useSafeSdk"
+import { useLayoutStore } from "../../../stores/useLayoutStore"
+import { usePlaygroundStore } from "../../../stores/usePlaygroundStore"
 
 const PendingTxCard = ({ tx }) => {
   console.log("tx", tx)
-  const { formattedDate, timeFromNow } = useRelativeTime()
+  const { timeFromNow } = useRelativeTime()
   const { transaction } = tx
   const { to } = transaction?.txInfo
   const bbyDao = usePlaygroundStore(state => state.expandedDao)
@@ -19,6 +19,9 @@ const PendingTxCard = ({ tx }) => {
   }, [tx.timestamp, tx?.transaction?.timestamp, timeFromNow])
 
   const status = React.useMemo(() => {
+    //SUCCESS
+    //AWAITING_EXECUTION
+    //AWAITING_CONFIRMATIONS
     return transaction?.txStatus === "SUCCESS" ? <HiCheckCircle size={28} /> : null
   }, [transaction])
 
@@ -32,50 +35,86 @@ const PendingTxCard = ({ tx }) => {
 
   const safeService = new SafeServiceClient("https://safe-transaction.gnosis.io")
   const [pendingTx, setPendingTx] = React.useState(undefined)
-  const bbyDaoTransaction = React.useMemo(async () => {
-    if(!!safeTxHash) {
-      const transaction = await safeService.getTransaction(safeTxHash)
-      setPendingTx(transaction)
+  React.useMemo(async () => {
+    try {
+      if (!!safeTxHash) {
+        const transaction = await safeService.getTransaction(safeTxHash)
+        setPendingTx(transaction)
+      }
+    } catch (err) {
+      console.log("err", err)
     }
-
   }, [safeTxHash, setPendingTx])
 
+  const camelToSpaceUpper = React.useCallback(camel => {
+    let result = camel?.replace(/([A-Z])/g, " $1").trim()
+    return result.charAt(0).toUpperCase() + result.slice(1)
+  }, [])
 
+  const safeSdk = useSafeSdk(bbyDao)
 
   return (
-    <div className="mb-4 flex flex-col space-x-2 rounded-xl bg-slate-200 p-3 dark:bg-slate-800">
-      <div className="ml-auto text-xs font-thin">{txTimeFromNow}</div>
-      <div className="text-xs font-thin">Nonce: {transaction?.executionInfo?.nonce}</div>
-      <div className="text-xs font-thin">Safe Tx: {safeTxHash.substring(safeTxHash.length - 6, safeTxHash.length)}</div>
-      <div>{transaction?.txInfo?.methodName || transaction?.txInfo?.dataDecoded?.method}</div>
-      <div className="flex flex-col">
-        <div className="flex">
-          <div className="mr-2 h-6 w-6 overflow-hidden rounded-full">
-            <img src={to?.logoUri} />
-          </div>
-          <div className="font-thin">{to?.name}</div>
-        </div>
-        <div className="mt-4 text-xs font-thin">
-          <div className="flex items-center">
-            Required Signatures:{" "}
-            <span className="max-w-4 ml-2 flex h-4 max-h-4 w-4 items-center justify-center rounded-full p-4 text-lg dark:bg-slate-700">
-              {transaction?.executionInfo?.confirmationsRequired}
-            </span>
-          </div>
-          <div className="flex items-center">
-            Total Signatures:{" "}
-            <span className="max-w-4 ml-2 flex h-4 max-h-4 w-4 items-center justify-center rounded-full p-4 text-lg dark:bg-slate-700">
-              {transaction?.executionInfo?.confirmationsSubmitted}
-            </span>
-          </div>
-        </div>
-        <div>{transaction?.txStatus}</div>
+    <div className="mb-2 flex flex-col rounded-xl bg-slate-200 p-3 dark:bg-slate-800">
+      <div className="mb-2 flex justify-between text-xs font-thin">
+        <div>{transaction?.executionInfo?.nonce}</div>
+        <div>{txTimeFromNow}</div>
       </div>
-      {isMissingSigner ? (
-        <button className="mt-4 inline-flex items-center self-start rounded px-4 py-2 font-thin dark:bg-orange-600 dark:hover:bg-orange-700">
-          Sign
-        </button>
-      ) : null}
+      {!!to?.name && (
+        <a
+          href={`https://etherscan.io/address/${to?.value}#code`}
+          className="flex flex-col rounded-xl bg-slate-300 p-4 dark:bg-slate-900"
+          target="_blank"
+        >
+          {!!to?.name && (
+            <div className="flex flex-col">
+              <div className="text-xs font-thin">Interacting With:</div>
+              <div className="flex items-center">
+                <div className="mr-2 h-6 w-6 overflow-hidden rounded-full">
+                  <img src={to?.logoUri} />
+                </div>
+                <div className="font-thin">{to?.name}</div>
+              </div>
+            </div>
+          )}
+          {(transaction?.txInfo?.methodName || transaction?.txInfo?.dataDecoded?.method) && (
+            <div className="mt-4 font-thin">
+              <div className="text-xs">action:</div>
+              <div className="flex">{transaction?.txInfo?.methodName || transaction?.txInfo?.dataDecoded?.method}</div>
+            </div>
+          )}
+        </a>
+      )}
+
+      <div className="mt-2 flex flex-col rounded-xl bg-slate-300 p-4 dark:bg-slate-900">
+        <div className="mb-2 text-xs font-thin">Status: {transaction?.txStatus}</div>
+        <div className="flex gap-1 text-sm font-thin">
+          <div>{transaction?.executionInfo?.confirmationsSubmitted}</div>
+          <div>out of</div>
+          <div>{transaction?.executionInfo?.confirmationsRequired}</div>
+          <div>required signatures.</div>
+        </div>
+        <div></div>
+      </div>
+
+      {/*<div className="text-xs font-thin">Safe Tx: {safeTxHash}</div>*/}
+
+      <div className="mt-2 flex gap-2">
+        {isMissingSigner ? (
+          <button className="mt-4 inline-flex items-center self-start rounded px-4 py-2 font-thin dark:bg-orange-600 dark:hover:bg-orange-700">
+            Sign
+          </button>
+        ) : (
+          <div className="inline-flex items-center gap-2 self-start rounded bg-slate-300 px-4 py-2 font-thin dark:bg-slate-900">
+            Signed
+            <HiCheckCircle />
+          </div>
+        )}
+        {!transaction?.txInfo?.isCancellation && (
+          <button className="inline-flex items-center self-start rounded bg-red-600 px-4 py-2 font-thin text-white hover:bg-red-700 dark:bg-rose-700 dark:hover:bg-rose-600">
+            Reject
+          </button>
+        )}
+      </div>
     </div>
   )
 }
