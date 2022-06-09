73c9447013bc06fda6fd23e63ee569ffdd83716d
Théo Mode <milesxbridges@gmail.com>
2022-06-09 18:31:15 -0400

    history card edits

--- components/Playground/Feed/HistoryTxCard.js
+++ components/Playground/Feed/HistoryTxCard.js
@@ -35,6 +35,111 @@ const HistoryTxCard = ({ tx }) => {
     return transaction?.id?.replace(`multisig_${bbyDao}_`, "")
   }, [bbyDao, transaction])
 
+  const [isExpanded, setExpanded] = React.useState(false)
+
+  const expandInfo = React.useCallback(() => {
+    if (!isExpanded) {
+      setExpanded(true)
+    }
+  }, [isExpanded])
+
+  const collapseInfo = React.useCallback(() => {
+    if (isExpanded) {
+      setExpanded(false)
+    }
+  }, [isExpanded])
+
+  const moreInfoButton = React.useMemo(() => {
+    return (
+        <button
+            type="button"
+            onClick={isExpanded ? collapseInfo : expandInfo}
+            className="mt-3 w-full rounded-xl bg-slate-300 py-1 dark:bg-slate-700"
+        >
+          {isExpanded ? "hide" : "more"} info
+        </button>
+    )
+  }, [isExpanded])
+
+  const expandedInfo = React.useMemo(() => {
+    return isExpanded ? (
+        <div className="flex flex-col px-1 py-3 lg:flex-row">
+          <div className={`w-full ${tx.txType === "MULTISIG_TRANSACTION" ? "lg:mr-2 lg:w-1/2" : ""}`}>
+            <ul className={`${tx.txType !== "MULTISIG_TRANSACTION" ? "flex justify-between flex-wrap" : ""}`}>
+              <li className={`${tx.txType !== "MULTISIG_TRANSACTION" ? "mt-2" : ""}`}>
+                <div>data:</div>
+                {tx.dataDecoded ? (
+                    <textarea
+                        className="h-16 w-full rounded-md border border-gray-300 p-2"
+                        disabled
+                        value={JSON.stringify(tx.dataDecoded)}
+                    />
+                ) : (
+                    <div>n/a</div>
+                )}
+              </li>
+              <li className="mt-2">
+                <div>nonce:</div>
+                {tx.nonce ? <div>{tx.nonce}</div> : <div>n/a</div>}
+              </li>
+              <li className="mt-2">
+                <div>gas used:</div>
+                <div>{tx.gasUed}</div>
+              </li>
+              <li className="mt-2">
+                <div>Sub. Date</div>
+                <div>{tx.submissionDate ? formattedDate(tx.submissionDate) : "n/a"}</div>
+              </li>
+              <li className="mt-2">
+                <div>Exec. Date</div>
+                <div>{tx.executionDate ? formattedDate(tx.executionDate) : "n/a"}</div>
+              </li>
+            </ul>
+          </div>
+          {tx.txType === "MULTISIG_TRANSACTION" ? (
+              <div className="mt-2 w-full lg:mt-0 lg:w-1/2">
+                <ul>
+                  <li>
+                    <div>approvals:</div>
+                    <div>
+                      <ul className="mt-2 flex">
+                        {tx.confirmations.map(({ owner }) => (
+                            <li className="mr-2 rounded-xl bg-slate-300 px-3 py-1 dark:bg-slate-700">
+                              <a
+                                  href={`https://etherscan.io/address/${owner}`}
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  className="text-sky-400 hover:underline"
+                              >
+                                {owner.replace("0x", "").slice(0, 6)}
+                              </a>
+                            </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </li>
+                  <li className="mt-2">
+                    <div>Executed by:</div>
+                    {tx.executor ? (
+                        <a
+                            href={`https://etherscan.io/address/${tx.executor}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-sky-400 hover:underline"
+                        >
+                          {tx.executor.replace("0x", "").slice(0, 6)}
+                        </a>
+                    ) : (
+                        <div>n/a</div>
+                    )}
+                  </li>
+                </ul>
+              </div>
+          ) : null}
+        </div>
+    ) : null
+  }, [tx, isExpanded])
+
   return (
     <div className="mb-2 flex flex-col rounded-xl bg-slate-200 p-3 dark:bg-slate-800">
       <div className="mb-2 flex justify-between text-xs font-thin">
@@ -80,7 +185,6 @@ const HistoryTxCard = ({ tx }) => {
           <div>required signatures.</div>
         </div>
       </div>
-      {}
     </div>
   )
 }
